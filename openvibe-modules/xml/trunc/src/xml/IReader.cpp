2d1d4d6cec197a00878a90c285398e239d68b22c
Yann Renard <dev@openvibe.inria.fr>
2006-12-07 10:01:42 +0000

    OpenViBE :  + added attributes on scnearii, boxes, and links (for gui use)  + added basic box addtion to the scenario (with no algorithm class provided)  * changed box / link enumeration interface so the callback gets non const reference object  * corrected minor bugs
    OpenViBE kernel :
     + loading functionnality implemented
     * corrected implementation according to openvibe interface changes
    
    OpenViBE module XML :
     + added XML reading functionnality based on expat (http://expat.sourceforge.net)
    
    
    
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@77 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

---
+++ openvibe-modules/xml/trunc/src/xml/IReader.cpp
@@ -0,0 +1,88 @@
+#include "IReader.h"
+
+#include <expat.h>
+#include <string>
+
+using namespace XML;
+using namespace std;
+
+namespace XML
+{
+	class CReader : virtual public IReader
+	{
+	public:
+		CReader(IReaderCallBack& rReaderCallBack);
+
+		virtual XML::boolean processData(const void* pBuffer, const uint64 ui64BufferSize);
+		virtual void release(void);
+
+	protected:
+
+		IReaderCallBack& m_rReaderCallBack;
+		::XML_Parser m_pXMLParser;
+	};
+
+	static void XMLCALL expat_xml_start(void* pData, const char* pElement, const char** ppAttribute);
+	static void XMLCALL expat_xml_end(void* pData, const char* pElement);
+	static void XMLCALL expat_xml_data(void* pData, const char* pDataValue, int iDataLength);
+};
+
+CReader::CReader(IReaderCallBack& rReaderCallBack)
+	:m_rReaderCallBack(rReaderCallBack)
+	,m_pXMLParser(NULL)
+{
+	m_pXMLParser=XML_ParserCreate(NULL);
+	XML_SetElementHandler(m_pXMLParser, expat_xml_start, expat_xml_end);
+	XML_SetCharacterDataHandler(m_pXMLParser, expat_xml_data);
+	XML_SetUserData(m_pXMLParser, &m_rReaderCallBack);
+}
+
+boolean CReader::processData(const void* pBuffer, const uint64 ui64BufferSize)
+{
+	XML_Status l_eStatus=XML_Parse(
+		m_pXMLParser,
+		static_cast<const char*>(pBuffer),
+		ui64BufferSize,
+		false);
+	return (l_eStatus==XML_STATUS_OK);
+}
+
+void CReader::release(void)
+{
+	delete this;
+}
+
+XML_API IReader* XML::createReader(IReaderCallBack& rReaderCallBack)
+{
+	return new CReader(rReaderCallBack);
+}
+
+static void XMLCALL XML::expat_xml_start(void* pData, const char* pElement, const char** ppAttribute)
+{
+	uint64 i, l_ui64AttributeCount=0;
+	while(ppAttribute[l_ui64AttributeCount++]);
+	l_ui64AttributeCount>>=1;
+	const char** l_pAttributeName=new const char*[l_ui64AttributeCount];
+	const char** l_pAttributeValue=new const char*[l_ui64AttributeCount];
+	for(i=0; i<l_ui64AttributeCount; i++)
+	{
+		l_pAttributeName[i]=ppAttribute[i<<1];
+		l_pAttributeValue[i]=ppAttribute[i<<1+1];
+	}
+
+	static_cast<IReaderCallBack*>(pData)->openChild(pElement, l_pAttributeName, l_pAttributeValue, l_ui64AttributeCount);
+
+	delete [] l_pAttributeName;
+	delete [] l_pAttributeValue;
+}
+
+static void XMLCALL XML::expat_xml_end(void* pData, const char* pElement)
+{
+	static_cast<IReaderCallBack*>(pData)->closeChild();
+}
+
+static void XMLCALL XML::expat_xml_data(void* pData, const char* pDataValue, int iDataLength)
+{
+	string sData(pDataValue, iDataLength);
+	static_cast<IReaderCallBack*>(pData)->processChildData(sData.c_str());
+}
