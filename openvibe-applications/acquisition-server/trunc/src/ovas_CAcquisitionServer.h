5d4c161f67ec5b8f758417a66a7faaa4f7f367ad
Yann Renard <dev@openvibe.inria.fr>
2007-03-02 15:04:35 +0000

    openvibe-application-acquisition-server :  * removed EBML node definition (replaced with openvibe-toolkit defined ones)
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@172 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- openvibe-applications/acquisition-server/trunc/src/ovas_CAcquisitionServer.h
+++ openvibe-applications/acquisition-server/trunc/src/ovas_CAcquisitionServer.h
@@ -18,25 +18,6 @@
 #include <list>
 #include <map>
 
-/******************************************************************************/
-
-#define EBML_Identifier_HEADER                        EBML::CIdentifier(0x04239)
-#define EBML_Identifier_INFORMATION                   EBML::CIdentifier(0x04240)
-#define EBML_Identifier_EXPERIMENT_ID                 EBML::CIdentifier(0x04241)
-#define EBML_Identifier_SUBJECT_AGE                   EBML::CIdentifier(0x04242)
-#define EBML_Identifier_SUBJECT_SEX                   EBML::CIdentifier(0x04243)
-#define EBML_Identifier_CHANNEL_NUMBER                EBML::CIdentifier(0x04244)
-#define EBML_Identifier_SAMPLING_FREQUENCY            EBML::CIdentifier(0x04245)
-#define EBML_Identifier_CHANNEL_NAME_LIST             EBML::CIdentifier(0x04246)
-#define EBML_Identifier_CHANNEL_NAME                  EBML::CIdentifier(0x04247)
-
-#define EBML_Identifier_BUFFER                        EBML::CIdentifier(0x05A)
-#define EBML_Identifier_SAMPLES                       EBML::CIdentifier(0x05B)
-#define EBML_Identifier_NUMBER_OF_SAMPLE_PER_CHANNELS EBML::CIdentifier(0x05C)
-#define EBML_Identifier_SAMPLE_BLOC                   EBML::CIdentifier(0x05D)
-
-/******************************************************************************/
-
 namespace OpenViBEAcquisitionServer
 {
 	class CConnectionHandler : virtual public EBML::IWriterCallback
