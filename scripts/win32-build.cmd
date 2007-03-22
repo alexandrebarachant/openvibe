07e202b31a5f30346dd8f8e57f5600631ecd309f
Yann Renard <dev@openvibe.inria.fr>
2007-03-22 17:45:22 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@297 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- scripts/win32-build.cmd
+++ scripts/win32-build.cmd
@@ -1,9 +1,24 @@
-call win32-init_env_command.cmd
+@echo off
 
-cd ..
-md local-tmp
-cd local-tmp
-cmake .. -G"NMake Makefiles"
-nmake
+if not exist "win32-init_env_command.cmd" (
+	echo You have to configure your `win32-init_env_command` file.
+	echo `win32-init_env_command-skeleton` is a good file to start with...
+	goto terminate
+)
+
+call "win32-init_env_command.cmd"
+
+set saved_directory=%CD%
+
+mkdir ..\local-tmp > NULL
+for /d %%s in (cmake\*.*) do (
+	mkdir ..\local-tmp\%%s > NULL
+	cd ..\local-tmp\%%s
+	cmake %saved_directory%/%%s -G"NMake Makefiles"
+	nmake
+	cd %saved_directory%
+)
+
+:terminate
 
 pause
--- scripts/win32-clean.cmd
+++ scripts/win32-clean.cmd
@@ -1,8 +1,25 @@
-call win32-init_env_command.cmd
+@echo off
 
-mkdir ..\local-tmp
-cd ..\local-tmp
-nmake clean
-cd ..
+if not exist "win32-init_env_command.cmd" (
+	echo You have to configure your `win32-init_env_command` file.
+	echo `win32-init_env_command-skeleton` is a good file to start with...
+	goto terminate
+)
 
-rmdir /s /q local-tmp
+call "win32-init_env_command.cmd"
+
+set saved_directory=%CD%
+
+mkdir ..\local-tmp > NULL
+for /d %%s in (cmake\*.*) do (
+	mkdir ..\local-tmp\%%s > NULL
+	cd ..\local-tmp\%%s
+	cmake %saved_directory%/%%s -G"NMake Makefiles"
+	nmake clean
+	cd %saved_directory%
+)
+rmdir /s /q ..\local-tmp
+
+:terminate
+
+pause
