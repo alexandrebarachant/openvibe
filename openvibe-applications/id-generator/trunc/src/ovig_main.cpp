73ddb0adb7f0b149331539ef9340fec0b1bf2c95
Yann Renard <dev@openvibe.inria.fr>
2007-06-01 13:11:26 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@475 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- openvibe-applications/id-generator/trunc/src/ovig_main.cpp
+++ openvibe-applications/id-generator/trunc/src/ovig_main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 	srand((unsigned int)time(NULL));
 	for(int i=0; i<15; i++)
 	{
-		unsigned long int l_ui32Vale1=(rand()&0x00ffffff);
+		unsigned long int l_ui32Vale1=(rand()&0xffffffff);
 		unsigned long int l_ui32Vale2=(rand()&0xffffffff);
 		printf("#define OV_ClassId_                                        OpenViBE::CIdentifier(0x%08X, 0x%08X)\n", (int)l_ui32Vale1, (int)l_ui32Vale2);
 	}
