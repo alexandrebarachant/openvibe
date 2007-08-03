7cb85986b2c4ee2e9d9499e100c77a40e65b7fe4
Yann Renard <dev@openvibe.inria.fr>
2007-08-03 14:00:40 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@623 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

---
+++ scripts/win32-generate-vc-proj.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+if not exist "win32-init_env_command.cmd" (
+	echo You have to configure your `win32-init_env_command` file.
+	echo `win32-init_env_command-skeleton` is a good file to start with...
+	goto terminate
+)
+
+call "win32-init_env_command.cmd"
+
+set saved_directory=%CD%
+
+mkdir ..\local-tmp 2> NULL
+del CMakeLists.txt 2> NULL
+
+for /d %%s in (cmake\*.*) do (
+	type %%s\CMakeLists.txt >> CMakeLists.txt
+)
+cd ..\local-tmp
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS=" /DWIN32 /D_WINDOWS /W3 /Zm1000 /EHsc /GR /wd4355" %saved_directory% -G"Visual Studio 8 2005"
+cd %saved_directory%
+del NULL
+
+:terminate
+
+pause
