a359f5fbd5cb7d74f9768654486d7f489a15dc9e
Yann Renard <dev@openvibe.inria.fr>
2007-10-05 12:35:21 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@710 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- cmake-modules/FindThirdPartyFabien.cmake
+++ cmake-modules/FindThirdPartyFabien.cmake
@@ -3,10 +3,16 @@
 # Adds library to target
 # Adds include path
 # ---------------------------------
-FIND_PATH(PATH_FABIEN_LIB include/Trial.h PATHS /local/yrenard/Open-ViBE/repository/libBCI)
+FIND_PATH(PATH_FABIEN_LIB include/Trial.h PATHS $ENV{OpenViBE_dependencies} /local/yrenard/Open-ViBE/repository/libBCI)
 IF(PATH_FABIEN_LIB)
 	MESSAGE(STATUS "  Found FABIEN_LIB...")
 	INCLUDE_DIRECTORIES(${PATH_FABIEN_LIB}/include)
+	INCLUDE_DIRECTORIES(${PATH_FABIEN_LIB}/../include/itpp)            # for itpp, Fabien does not include files correctly
+	INCLUDE_DIRECTORIES(${PATH_FABIEN_LIB}/../include/itpp/base)       # for itpp, Fabien does not include files correctly
+	INCLUDE_DIRECTORIES(${PATH_FABIEN_LIB}/../include/itpp/comm)       # for itpp, Fabien does not include files correctly
+	INCLUDE_DIRECTORIES(${PATH_FABIEN_LIB}/../include/itpp/fixedpoint) # for itpp, Fabien does not include files correctly
+	INCLUDE_DIRECTORIES(${PATH_FABIEN_LIB}/../include/itpp/protocol)   # for itpp, Fabien does not include files correctly
+	INCLUDE_DIRECTORIES(${PATH_FABIEN_LIB}/../include/itpp/srccode)    # for itpp, Fabien does not include files correctly
 	FIND_LIBRARY(LIB_FABIEN_LIB OpenViBE PATHS ${PATH_FABIEN_LIB}/lib )
 	IF(LIB_FABIEN_LIB)
 		MESSAGE(STATUS "    [  OK  ] lib ${LIB_FABIEN_LIB}")
