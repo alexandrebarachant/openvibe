33c22170eb00fbbb7820369961aa8de57d0281df
Yann Renard <dev@openvibe.inria.fr>
2008-03-17 18:07:09 +0000

    scripts / global :  * solved portability issues on windows
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@1060 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- scripts/win32-clean.cmd
+++ scripts/win32-clean.cmd
@@ -1,5 +1,22 @@
 @echo off
 
+if "%1" == "" goto base
+
+set what_full=%1
+echo set what_relative=%%what_full:%OpenViBE_base%\=%% > windows_sucks.cmd
+call windows_sucks.cmd
+del windows_sucks.cmd
+
+mkdir ..\local-tmp\%what_relative% 2> NULL
+cd ..\local-tmp\%what_relative%
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=" /DWIN32 /D_WINDOWS /W3 /Zm1000 /EHsc /GR /wd4355" -DCMAKE_MODULE_PATH="%saved_directory:\=/%/../cmake-modules;${CMAKE_MODULE_PATH}" %what_full% -G"NMake Makefiles"
+nmake clean
+cd %saved_directory%
+
+goto terminate_2
+
+:base
+
 if not exist "win32-init_env_command.cmd" (
 	echo You have to configure your `win32-init_env_command` file.
 	echo `win32-init_env_command-skeleton` is a good file to start with...
@@ -11,16 +28,13 @@ call "win32-init_env_command.cmd"
 set saved_directory=%CD%
 
 mkdir ..\local-tmp 2> NULL
-for /d %%s in (cmake\*.*) do (
-	mkdir ..\local-tmp\%%s 2> NULL
-	cd ..\local-tmp\%%s
-	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=" /DWIN32 /D_WINDOWS /W3 /Zm1000 /EHsc /GR /wd4355" -DCMAKE_MODULE_PATH="%saved_directory:\=/%/../cmake-modules;${CMAKE_MODULE_PATH}" %saved_directory%/%%s -G"NMake Makefiles"
-	nmake clean
-	cd %saved_directory%
+for /F %%s in (%OpenViBE_build_order%) do (
+	cmd /e /c %0 %%s
 )
-rmdir /s /q ..\local-tmp
 del NULL
 
 :terminate
 
 pause
+
+:terminate_2
--- scripts/win32-collect_build.cmd
+++ scripts/win32-collect_build.cmd
@@ -60,9 +60,9 @@ xcopy /q /s %OpenViBE_module_xml%\bin\*.exe                     %target_dist%\bi
 xcopy /q /s %OpenViBE%\bin\*.exe                                %target_dist%\bin     > NULL 2<&1
 xcopy /q /s %OpenViBE_kernel%\bin\*.exe                         %target_dist%\bin     > NULL 2<&1
 xcopy /q /s %OpenViBE_toolkit%\bin\*.exe                        %target_dist%\bin     > NULL 2<&1
-xcopy /q /s %OpenViBE_designer%\bin\*.exe                       %target_dist%\bin     > NULL 2<&1
-xcopy /q /s %OpenViBE_acquisition_server%\bin\*.exe             %target_dist%\bin     > NULL 2<&1
-xcopy /q /s %OpenViBE_id_generator%\bin\*.exe                   %target_dist%\bin     > NULL 2<&1
+xcopy /q /s %OpenViBE_application_designer%\bin\*.exe           %target_dist%\bin     > NULL 2<&1
+xcopy /q /s %OpenViBE_application_acquisition_server%\bin\*.exe %target_dist%\bin     > NULL 2<&1
+xcopy /q /s %OpenViBE_application_id_generator%\bin\*.exe       %target_dist%\bin     > NULL 2<&1
 
 xcopy /q /s %OpenViBE_module_ebml%\include\*.*                  %target_dist%\include > NULL 2<&1
 xcopy /q /s %OpenViBE_module_automaton%\include\*.*             %target_dist%\include > NULL 2<&1
@@ -73,9 +73,9 @@ xcopy /q /s %OpenViBE_module_xml%\include\*.*                   %target_dist%\in
 xcopy /q /s %OpenViBE%\include\*.*                              %target_dist%\include > NULL 2<&1
 xcopy /q /s %OpenViBE_kernel%\include\*.*                       %target_dist%\include > NULL 2<&1
 xcopy /q /s %OpenViBE_toolkit%\include\*.*                      %target_dist%\include > NULL 2<&1
-xcopy /q /s %OpenViBE_designer%\include\*.*                     %target_dist%\include > NULL 2<&1
-xcopy /q /s %OpenViBE_acquisition_server%\include\*.*           %target_dist%\include > NULL 2<&1
-xcopy /q /s %OpenViBE_id_generator%\include\*.*                 %target_dist%\include > NULL 2<&1
+xcopy /q /s %OpenViBE_application_designer%\include\*.*         %target_dist%\include > NULL 2<&1
+xcopy /q /s %OpenViBE_application_acquisition_server%\include\*.* %target_dist%\include > NULL 2<&1
+xcopy /q /s %OpenViBE_application_id_generator%\include\*.*     %target_dist%\include > NULL 2<&1
 
 xcopy /q /s %OpenViBE_module_automaton%\lib\*.lib               %target_dist%\lib     > NULL 2<&1
 xcopy /q /s %OpenViBE_module_automaton%\lib\*.dll               %target_dist%\lib     > NULL 2<&1
@@ -95,12 +95,12 @@ xcopy /q /s %OpenViBE_kernel%\lib\*.lib                         %target_dist%\li
 xcopy /q /s %OpenViBE_kernel%\lib\*.dll                         %target_dist%\lib     > NULL 2<&1
 xcopy /q /s %OpenViBE_toolkit%\lib\*.lib                        %target_dist%\lib     > NULL 2<&1
 xcopy /q /s %OpenViBE_toolkit%\lib\*.dll                        %target_dist%\lib     > NULL 2<&1
-xcopy /q /s %OpenViBE_designer%\lib\*.lib                       %target_dist%\lib     > NULL 2<&1
-xcopy /q /s %OpenViBE_designer%\lib\*.dll                       %target_dist%\lib     > NULL 2<&1
-xcopy /q /s %OpenViBE_acquisition_server%\lib\*.lib             %target_dist%\lib     > NULL 2<&1
-xcopy /q /s %OpenViBE_acquisition_server%\lib\*.dll             %target_dist%\lib     > NULL 2<&1
-xcopy /q /s %OpenViBE_id_generator%\lib\*.lib                   %target_dist%\lib     > NULL 2<&1
-xcopy /q /s %OpenViBE_id_generator%\lib\*.dll                   %target_dist%\lib     > NULL 2<&1
+xcopy /q /s %OpenViBE_application_designer%\lib\*.lib           %target_dist%\lib     > NULL 2<&1
+xcopy /q /s %OpenViBE_application_designer%\lib\*.dll           %target_dist%\lib     > NULL 2<&1
+xcopy /q /s %OpenViBE_application_acquisition_server%\lib\*.lib %target_dist%\lib     > NULL 2<&1
+xcopy /q /s %OpenViBE_application_acquisition_server%\lib\*.dll %target_dist%\lib     > NULL 2<&1
+xcopy /q /s %OpenViBE_application_id_generator%\lib\*.lib       %target_dist%\lib     > NULL 2<&1
+xcopy /q /s %OpenViBE_application_id_generator%\lib\*.dll       %target_dist%\lib     > NULL 2<&1
 
 xcopy /q /s %OpenViBE_module_ebml%\share\*.*                    %target_dist%\share   > NULL 2<&1
 xcopy /q /s %OpenViBE_module_automaton%\share\*.*               %target_dist%\share   > NULL 2<&1
@@ -111,9 +111,9 @@ xcopy /q /s %OpenViBE_module_xml%\share\*.*                     %target_dist%\sh
 xcopy /q /s %OpenViBE%\share\*.*                                %target_dist%\share   > NULL 2<&1
 xcopy /q /s %OpenViBE_kernel%\share\*.*                         %target_dist%\share   > NULL 2<&1
 xcopy /q /s %OpenViBE_toolkit%\share\*.*                        %target_dist%\share   > NULL 2<&1
-xcopy /q /s %OpenViBE_designer%\share\*.*                       %target_dist%\share   > NULL 2<&1
-xcopy /q /s %OpenViBE_acquisition_server%\share\*.*             %target_dist%\share   > NULL 2<&1
-xcopy /q /s %OpenViBE_id_generator%\share\*.*                   %target_dist%\share   > NULL 2<&1
+xcopy /q /s %OpenViBE_application_designer%\share\*.*           %target_dist%\share   > NULL 2<&1
+xcopy /q /s %OpenViBE_application_acquisition_server%\share\*.* %target_dist%\share   > NULL 2<&1
+xcopy /q /s %OpenViBE_application_id_generator%\share\*.*       %target_dist%\share   > NULL 2<&1
 
 REM #######################################################################################
 
