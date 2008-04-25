1390821c44b84347b08f1cecd1fc565051350d81
Yann Renard <dev@openvibe.inria.fr>
2008-04-25 10:10:28 +0000

    merged wip-ggibert with trunc resulting in the following :
    openvibe-plugins-tools :
     + added mouse cursor control box algorithm
    
    
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@1092 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- openvibe-plugins/tools/trunc/src/ovp_main.cpp
+++ openvibe-plugins/tools/trunc/src/ovp_main.cpp
@@ -1,34 +1,9 @@
 #include "ovp_defines.h"
+
 #include "ovpCEBMLStreamSpy.h"
+#include "ovpCMouseControl.h"
 
-static OpenViBEPlugins::Tools::CEBMLStreamSpyDesc* gst_pEBMLStreamSpyDesc=NULL;
-
-extern "C"
-{
-
-OVP_API OpenViBE::boolean onInitialize(const OpenViBE::Kernel::IPluginModuleContext& rPluginModuleContext)
-{
-	gst_pEBMLStreamSpyDesc=new OpenViBEPlugins::Tools::CEBMLStreamSpyDesc();
-
-	return true;
-}
-
-OVP_API OpenViBE::boolean onUninitialize(const OpenViBE::Kernel::IPluginModuleContext& rPluginModuleContext)
-{
-	delete gst_pEBMLStreamSpyDesc;
-
-	return true;
-}
-
-OVP_API OpenViBE::boolean onGetPluginObjectDescription(const OpenViBE::Kernel::IPluginModuleContext& rPluginModuleContext, OpenViBE::uint32 ui32Index, OpenViBE::Plugins::IPluginObjectDesc*& rpPluginObjectDescription)
-{
-	switch(ui32Index)
-	{
-		case 0: rpPluginObjectDescription=gst_pEBMLStreamSpyDesc; break;
-
-		default: return false;
-	}
-	return true;
-}
-
-}
+OVP_Declare_Begin();
+	OVP_Declare_New(OpenViBEPlugins::Tools::CEBMLStreamSpyDesc);
+	OVP_Declare_New(OpenViBEPlugins::Tools::CMouseControlDesc);
+OVP_Declare_End();
