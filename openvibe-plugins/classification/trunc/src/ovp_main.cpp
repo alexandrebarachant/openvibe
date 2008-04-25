4eecec6f663a06b130463365552015c90e44e846
Yann Renard <dev@openvibe.inria.fr>
2008-04-25 10:11:08 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@1095 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- openvibe-plugins/classification/trunc/src/ovp_main.cpp
+++ openvibe-plugins/classification/trunc/src/ovp_main.cpp
@@ -1,37 +1,7 @@
+#include "ovp_defines.h"
+
 #include "ovpCLDAClassifier.h"
 
-#include "ovp_defines.h"
-#include <openvibe/ov_all.h>
-
-static OpenViBEPlugins::Classification::CLDAClassifierDesc* gst_pLDACLassifierDesc=NULL;
-
-extern "C"
-{
-
-OVP_API OpenViBE::boolean onInitialize(const OpenViBE::Kernel::IPluginModuleContext& rPluginModuleContext)
-{
-	gst_pLDACLassifierDesc = new OpenViBEPlugins::Classification::CLDAClassifierDesc();
-
-	return true;
-}
-
-OVP_API OpenViBE::boolean onUninitialize(const OpenViBE::Kernel::IPluginModuleContext& rPluginModuleContext)
-{
-	delete gst_pLDACLassifierDesc;
-
-	return true;
-}
-
-OVP_API OpenViBE::boolean onGetPluginObjectDescription(const OpenViBE::Kernel::IPluginModuleContext& rPluginModuleContext, OpenViBE::uint32 ui32Index, OpenViBE::Plugins::IPluginObjectDesc*& rpPluginObjectDescription)
-{
-
-	switch(ui32Index)
-	{
-		case 0: rpPluginObjectDescription = gst_pLDACLassifierDesc; break;
-
-		default: return false;
-	}
-	return true;
-}
-
-}
+OVP_Declare_Begin();
+	OVP_Declare_New(OpenViBEPlugins::Classification::CLDAClassifierDesc);
+OVP_Declare_End();
--- openvibe-plugins/feature-extraction/trunc/src/ovp_main.cpp
+++ openvibe-plugins/feature-extraction/trunc/src/ovp_main.cpp
@@ -1,37 +1,7 @@
+#include "ovp_defines.h"
+
 #include "ovpCFeatureAggregator.h"
 
-#include "ovp_defines.h"
-#include <openvibe/ov_all.h>
-
-static OpenViBEPlugins::FeatureExtraction::CFeatureAggregatorDesc* gst_pFeatureAggregatorDesc=NULL;
-
-extern "C"
-{
-
-OVP_API OpenViBE::boolean onInitialize(const OpenViBE::Kernel::IPluginModuleContext& rPluginModuleContext)
-{
-	gst_pFeatureAggregatorDesc = new OpenViBEPlugins::FeatureExtraction::CFeatureAggregatorDesc();
-
-	return true;
-}
-
-OVP_API OpenViBE::boolean onUninitialize(const OpenViBE::Kernel::IPluginModuleContext& rPluginModuleContext)
-{
-	delete gst_pFeatureAggregatorDesc;
-
-	return true;
-}
-
-OVP_API OpenViBE::boolean onGetPluginObjectDescription(const OpenViBE::Kernel::IPluginModuleContext& rPluginModuleContext, OpenViBE::uint32 ui32Index, OpenViBE::Plugins::IPluginObjectDesc*& rpPluginObjectDescription)
-{
-
-	switch(ui32Index)
-	{
-		case 0: rpPluginObjectDescription = gst_pFeatureAggregatorDesc; break;
-
-		default: return false;
-	}
-	return true;
-}
-
-}
+OVP_Declare_Begin();
+	OVP_Declare_New(OpenViBEPlugins::FeatureExtraction::CFeatureAggregatorDesc);
+OVP_Declare_End();
