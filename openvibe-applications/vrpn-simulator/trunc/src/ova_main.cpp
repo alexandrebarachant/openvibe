9d810d8c0d2b6a3a9d0362c6a42ed125cdac991a
Yann Renard <dev@openvibe.inria.fr>
2008-05-19 17:27:57 +0000

    openvibe-applications-vrpn-simulator :  + added simple GTK based VPRN server in order to simulate buttons and analog values easily
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@1108 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

---
+++ openvibe-applications/vrpn-simulator/trunc/src/ova_main.cpp
@@ -0,0 +1,95 @@
+#include <glade/glade.h>
+
+#include <gtk/gtk.h>
+
+#include <gdk/gdk.h>
+
+#include <vrpn_Button.h>
+#include <vrpn_Analog.h>
+
+#include <iostream>
+
+#define _vrpn_peripheral_name_ "openvibe-vrpn@localhost"
+
+// #define _DEBUG
+
+::vrpn_Connection* g_pConnection=NULL;
+::vrpn_Button_Server* g_pButtonServer=NULL;
+::vrpn_Analog_Server* g_pAnalogServer=NULL;
+int g_iAnalogCount=0;
+int g_iButtonCount=0;
+
+void fScrollCB(::GtkRange* pRange, gpointer pUserData)
+{
+	g_pAnalogServer->channels()[(int)(pUserData)]=gtk_range_get_value(pRange);
+
+#if defined _DEBUG
+	std::cout << (int)(pUserData) << " value changed\n";
+#endif
+}
+
+void fSwitchCB(::GtkToggleButton* pTogglebutton, gpointer pUserData)
+{
+	g_pButtonServer->set_button((int)(pUserData), gtk_toggle_button_get_active(pTogglebutton));
+
+#if defined _DEBUG
+	std::cout << (int)(pUserData) << " toggled\n";
+#endif
+}
+
+void fConnectCB(::GtkWidget* pWidget, gpointer data)
+{
+	if(GTK_IS_RANGE(pWidget))
+	{
+		g_signal_connect(G_OBJECT(pWidget), "value-changed", G_CALLBACK(fScrollCB), (void*)g_iAnalogCount);
+		g_iAnalogCount++;
+	}
+
+	if(GTK_IS_TOGGLE_BUTTON(pWidget))
+	{
+		g_signal_connect(G_OBJECT(pWidget), "toggled", G_CALLBACK(fSwitchCB), (void*)g_iButtonCount);
+		g_iButtonCount++;
+	}
+}
+
+gboolean fIdleApplicationLoop(gpointer pUserData)
+{
+	g_pButtonServer->mainloop();
+	g_pAnalogServer->report_changes();
+	g_pAnalogServer->mainloop();
+	g_pConnection->mainloop();
+	return TRUE;
+}
+
+int main(int argc, char ** argv)
+{
+	gtk_init(&argc, &argv);
+	g_pConnection=new ::vrpn_Connection;
+	g_pButtonServer=new ::vrpn_Button_Server(_vrpn_peripheral_name_, g_pConnection, 10);
+	g_pAnalogServer=new ::vrpn_Analog_Server(_vrpn_peripheral_name_, g_pConnection);
+	g_pAnalogServer->setNumChannels(10);
+
+	::GladeXML* l_pInterface=glade_xml_new("../share/openvibe-applications/vrpn-simulator/interface.glade", "window", NULL);
+	::GtkWidget* l_pMainWindow=glade_xml_get_widget(l_pInterface, "window");
+	::GtkWidget* l_pHBoxButton=glade_xml_get_widget(l_pInterface, "hbox_button");
+	::GtkWidget* l_pHBoxAnalog=glade_xml_get_widget(l_pInterface, "hbox_analog");
+
+	g_signal_connect(G_OBJECT(l_pMainWindow), "destroy", gtk_main_quit, NULL);
+	gtk_container_foreach(GTK_CONTAINER(l_pHBoxButton), fConnectCB, NULL);
+	gtk_container_foreach(GTK_CONTAINER(l_pHBoxAnalog), fConnectCB, NULL);
+	glade_xml_signal_autoconnect(l_pInterface);
+
+	std::cout << "got " << g_iAnalogCount << " analogs...\n";
+	std::cout << "got " << g_iButtonCount << " buttons...\n";
+
+	g_idle_add(fIdleApplicationLoop, NULL);
+
+	gtk_widget_show(l_pMainWindow);
+	gtk_main();
+
+	delete g_pAnalogServer;
+	delete g_pButtonServer;
+	delete g_pConnection;
+
+	return 0;
+}
