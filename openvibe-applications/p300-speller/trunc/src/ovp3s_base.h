62ff82012dcad364134ab79b8d1d37183dc687a7
Yann Renard <dev@openvibe.inria.fr>
2008-12-11 16:06:04 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@1433 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

---
+++ openvibe-applications/p300-speller/trunc/src/ovp3s_base.h
@@ -0,0 +1,13 @@
+#ifndef __OpenViBEP300Speller_base_H__
+#define __OpenViBEP300Speller_base_H__
+
+#include <openvibe/ov_all.h>
+#include <openvibe-toolkit/ovtk_all.h>
+
+#include <glade/glade.h>
+#include <gtk/gtk.h>
+#include <gdk/gdk.h>
+
+#include "ovp3s_defines.h"
+
+#endif // __OpenViBEP300Speller_base_H__
---
+++ openvibe-applications/p300-speller/trunc/src/ovp3s_main.cpp
@@ -0,0 +1,14 @@
+#include "ovp3s_base.h"
+
+#include <iostream>
+
+using namespace OpenViBE;
+using namespace OpenViBE::Kernel;
+using namespace OpenViBE::Plugins;
+
+int main(int argc, char ** argv)
+{
+	std::cout << "This is not implemented yer\n"
+
+	return 0;
+}
