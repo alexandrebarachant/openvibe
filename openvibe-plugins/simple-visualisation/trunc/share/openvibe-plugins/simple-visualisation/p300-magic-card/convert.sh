1f9943e4cadfa2270a3f6706de8151d02b334c88
Yann Renard <dev@openvibe.inria.fr>
2009-09-23 15:08:35 +0000

    openvibe-plugins-simple-visualisation :  * updated p300 magic card images
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@1692 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- openvibe-plugins/simple-visualisation/trunc/share/openvibe-plugins/simple-visualisation/p300-magic-card/convert.sh
+++ openvibe-plugins/simple-visualisation/trunc/share/openvibe-plugins/simple-visualisation/p300-magic-card/convert.sh
@@ -4,7 +4,7 @@ for f in *.png; do
 	is_dest=`echo $f | grep offscreen`
 	if [ "$is_dest" = "" ]; then
 		echo working on $f...
-		convert "$f" -sepia-tone 90% -modulate 45% "$f-offscreen"
+		convert "$f" -sepia-tone 70% -modulate 25% "$f-offscreen"
 	fi
 done
 
