606ac02b0e45805b7cfed0329f19a9ab31160beb
Yann Renard <dev@openvibe.inria.fr>
2010-03-29 16:47:28 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@1986 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/cmake-modules/FindThirdPartyBoost.cmake
+++ trunk/cmake-modules/FindThirdPartyBoost.cmake
@@ -4,32 +4,14 @@
 # Adds include path
 # ---------------------------------
 
-IF(UNIX)
-	SET(Boost_ADDITIONAL_VERSIONS 1.34.1)
-	FIND_PACKAGE(Boost 1.36.0)
-	FIND_PACKAGE(Boost 1.36.0)
+FIND_PATH(PATH_BOOST "include/boost/config/auto_link.hpp" PATHS $ENV{OV_DEP_BOOST} $ENV{OpenViBE_dependencies} NO_DEFAULT_PATH)
+FIND_PATH(PATH_BOOST "include/boost/config/auto_link.hpp" PATHS $ENV{OV_DEP_BOOST})
 
-	IF(Boost_FOUND)
-		MESSAGE(STATUS "  Found boost...")
-		INCLUDE_DIRECTORIES(${Boost_INCLUDE_DIRS})
-#		LINK_DIRECTORIES(${Boost_LIBRARY_DIRS})
+IF(PATH_BOOST)
+	MESSAGE(STATUS "  Found boost...")
+	INCLUDE_DIRECTORIES(${PATH_BOOST}/include)
 
-		ADD_DEFINITIONS(-DTARGET_HAS_Boost)
-	ELSE(Boost_FOUND)
-		MESSAGE(STATUS "  FAILED to find boost...")
-	ENDIF(Boost_FOUND)
-ENDIF(UNIX)
-
-IF(WIN32)
-	FIND_PATH(PATH_BOOST "include/boost/config/auto_link.hpp" PATHS $ENV{OV_DEP_BOOST} NO_DEFAULT_PATH)
-	FIND_PATH(PATH_BOOST "include/boost/config/auto_link.hpp" PATHS $ENV{OV_DEP_BOOST})
-
-	IF(PATH_BOOST)
-		MESSAGE(STATUS "  Found boost...")
-		INCLUDE_DIRECTORIES(${PATH_BOOST}/include)
-
-		ADD_DEFINITIONS(-DTARGET_HAS_Boost)
-	ELSE(PATH_BOOST)
-		MESSAGE(STATUS "  FAILED to find boost...")
-	ENDIF(PATH_BOOST)
-ENDIF(WIN32)
+	ADD_DEFINITIONS(-DTARGET_HAS_Boost)
+ELSE(PATH_BOOST)
+	MESSAGE(STATUS "  FAILED to find boost...")
+ENDIF(PATH_BOOST)
