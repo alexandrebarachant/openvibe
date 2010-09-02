f12df79512f86ddbaae059ff330aa9eb20235056
Yann Renard <dev@openvibe.inria.fr>
2010-09-02 09:01:06 +0000

    openvibe-applications-acquisition-server :  + added drift correction on micromed driver
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@2319 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/openvibe-applications/acquisition-server/trunc/src/micromed-intraeeg/ovasCDriverMicromedIntraEEG.cpp
+++ trunk/openvibe-applications/acquisition-server/trunc/src/micromed-intraeeg/ovasCDriverMicromedIntraEEG.cpp
@@ -551,6 +551,7 @@ boolean CDriverMicromedIntraEEG::loop(void)
 				if(l_ui32nbSamplesBlock==m_ui32BuffDataIndex*m_oHeader.getChannelCount())
 				{
 					m_pCallback->setSamples(m_pSample);
+					m_rDriverContext.correctDriftSampleCount(m_rDriverContext.getSuggestedDriftCorrectionSampleCount());
 					m_rDriverContext.getLogManager() << LogLevel_Debug << "Send samples back to CAcquisitionServer: samples.len = " << m_ui32BuffDataIndex << "\n";
 					m_ui32BuffDataIndex=0;
 				}
