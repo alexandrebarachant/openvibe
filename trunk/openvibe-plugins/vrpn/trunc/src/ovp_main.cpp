7c2d700b3de5d46cb1edc4e57001736a576714de
Yann Renard <dev@openvibe.inria.fr>
2011-02-21 15:21:46 +0000

    openvibe-plugins-vrpn  + added analog vrpn client
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@2689 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/openvibe-plugins/vrpn/trunc/src/ovp_main.cpp
+++ trunk/openvibe-plugins/vrpn/trunc/src/ovp_main.cpp
@@ -3,6 +3,7 @@
 #include "ovp_defines.h"
 #include "ovpCVRPNAnalogServer.h"
 #include "ovpCVRPNButtonServer.h"
+#include "ovpCBoxAlgorithmVRPNAnalogClient.h"
 #include "ovpCBoxAlgorithmVRPNButtonClient.h"
 
 #include <vector>
@@ -12,6 +13,7 @@ OVP_Declare_Begin();
 #if defined TARGET_HAS_ThirdPartyVRPN
 	OVP_Declare_New(OpenViBEPlugins::VRPN::CVRPNAnalogServerDesc);
 	OVP_Declare_New(OpenViBEPlugins::VRPN::CVRPNButtonServerDesc);
+	OVP_Declare_New(OpenViBEPlugins::VRPN::CBoxAlgorithmVRPNAnalogClientDesc);
 	OVP_Declare_New(OpenViBEPlugins::VRPN::CBoxAlgorithmVRPNButtonClientDesc);
 #endif
 
