5747686f33b775cb0545501f861b5cd26faa6ab3
Yann Renard <dev@openvibe.inria.fr>
2011-03-22 09:00:55 +0000

    openvibe-plugins-streaming :  + added Signal Merger documentation  + added Streamed Matrix Multiplexer documentation  * Completely reimplemented Signal Merger documentation
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@2795 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/openvibe-plugins/streaming/trunc/src/box-algorithms/streaming/ovpCBoxAlgorithmSignalMerger.cpp
+++ trunk/openvibe-plugins/streaming/trunc/src/box-algorithms/streaming/ovpCBoxAlgorithmSignalMerger.cpp
@@ -1,6 +1,7 @@
 #include "ovpCBoxAlgorithmSignalMerger.h"
 
-#include <iostream>
+#include <system/Memory.h>
+
 using namespace OpenViBE;
 using namespace OpenViBE::Kernel;
 using namespace OpenViBE::Plugins;
@@ -8,196 +9,166 @@ using namespace OpenViBE::Plugins;
 using namespace OpenViBEPlugins;
 using namespace OpenViBEPlugins::Streaming;
 
-enum
-{
-	DecoderType_Unknown=0,
-	DecoderType_Header=1,
-	DecoderType_Buffer=2,
-	DecoderType_End=3,
-	DecoderType_Error=10,
-};
-
 boolean CBoxAlgorithmSignalMerger::initialize(void)
 {
-	m_pSignalDecoder=&this->getAlgorithmManager().getAlgorithm(this->getAlgorithmManager().createAlgorithm(OVP_GD_ClassId_Algorithm_SignalStreamDecoder));
-	m_pSignalDecoder->initialize();
-	ip_pMemoryBufferToDecode1.initialize(m_pSignalDecoder->getInputParameter(OVP_GD_Algorithm_SignalStreamDecoder_InputParameterId_MemoryBufferToDecode));
-	op_pDecodedMatrix1.initialize(m_pSignalDecoder->getOutputParameter(OVP_GD_Algorithm_SignalStreamDecoder_OutputParameterId_Matrix));
-	op_ui64SamplingRate1.initialize(m_pSignalDecoder->getOutputParameter(OVP_GD_Algorithm_SignalStreamDecoder_OutputParameterId_SamplingRate));
+	IBox& l_rStaticBoxContext=this->getStaticBoxContext();
 
-	m_pSignalEncoder=&this->getAlgorithmManager().getAlgorithm(this->getAlgorithmManager().createAlgorithm(OVP_GD_ClassId_Algorithm_SignalStreamEncoder));
-	m_pSignalEncoder->initialize();
-	ip_pMatrixToEncode.initialize(m_pSignalEncoder->getInputParameter(OVP_GD_Algorithm_SignalStreamEncoder_InputParameterId_Matrix));
-	op_pEncodedMemoryBuffer.initialize(m_pSignalEncoder->getOutputParameter(OVP_GD_Algorithm_SignalStreamEncoder_OutputParameterId_EncodedMemoryBuffer));
-	ip_ui64SamplingRate.initialize(m_pSignalEncoder->getInputParameter(OVP_GD_Algorithm_SignalStreamEncoder_InputParameterId_SamplingRate));
+	for(uint32 i=0; i<l_rStaticBoxContext.getInputCount(); i++)
+	{
+		IAlgorithmProxy* l_pStreamDecoder=&this->getAlgorithmManager().getAlgorithm(this->getAlgorithmManager().createAlgorithm(OVP_GD_ClassId_Algorithm_SignalStreamDecoder));
+		l_pStreamDecoder->initialize();
+		m_vStreamDecoder.push_back(l_pStreamDecoder);
+	}
 
-	ip_ui64SamplingRate.setReferenceTarget(op_ui64SamplingRate1);
+	m_pStreamEncoder=&this->getAlgorithmManager().getAlgorithm(this->getAlgorithmManager().createAlgorithm(OVP_GD_ClassId_Algorithm_SignalStreamEncoder));
+	m_pStreamEncoder->initialize();
+
+	TParameterHandler < uint64 > ip_ui64SamplingRate(m_pStreamEncoder->getInputParameter(OVP_GD_Algorithm_SignalStreamEncoder_InputParameterId_SamplingRate));
+	TParameterHandler < uint64 > op_ui64SamplingRate(m_vStreamDecoder[0]->getOutputParameter(OVP_GD_Algorithm_SignalStreamDecoder_OutputParameterId_SamplingRate));
+	ip_ui64SamplingRate.setReferenceTarget(op_ui64SamplingRate);
+
 	return true;
 }
 
 boolean CBoxAlgorithmSignalMerger::uninitialize(void)
 {
-	op_pDecodedMatrix1.uninitialize();
-	ip_pMemoryBufferToDecode1.uninitialize();
-	op_ui64SamplingRate1.uninitialize();
-	m_pSignalDecoder->uninitialize();
-	this->getAlgorithmManager().releaseAlgorithm(*m_pSignalDecoder);
+	IBox& l_rStaticBoxContext=this->getStaticBoxContext();
 
-	ip_pMatrixToEncode.uninitialize();
-	op_pEncodedMemoryBuffer.uninitialize();
-	ip_ui64SamplingRate.uninitialize();
-	m_pSignalEncoder->uninitialize();
-	this->getAlgorithmManager().releaseAlgorithm(*m_pSignalEncoder);
+	m_pStreamEncoder->uninitialize();
+	this->getAlgorithmManager().releaseAlgorithm(*m_pStreamEncoder);
+
+	for(uint32 i=0; i<l_rStaticBoxContext.getInputCount(); i++)
+	{
+		IAlgorithmProxy* l_pStreamDecoder=m_vStreamDecoder[i];
+		l_pStreamDecoder->uninitialize();
+		this->getAlgorithmManager().releaseAlgorithm(*l_pStreamDecoder);
+	}
+	m_vStreamDecoder.clear();
 
 	return true;
 }
 
 boolean CBoxAlgorithmSignalMerger::processInput(uint32 ui32InputIndex)
 {
-	getBoxAlgorithmContext()->markAlgorithmAsReadyToProcess();
+	IBox& l_rStaticBoxContext=this->getStaticBoxContext();
+	IDynamicBoxContext& l_rDynamicBoxContext=this->getDynamicBoxContext();
+
+	if(l_rDynamicBoxContext.getInputChunkCount(0) == 0)
+	{
+		return true;
+	}
+
+	uint64 l_ui64StartTime=l_rDynamicBoxContext.getInputChunkStartTime(0, 0);
+	uint64 l_ui64EndTime=l_rDynamicBoxContext.getInputChunkEndTime(0, 0);
+	for(uint32 i=1; i<l_rStaticBoxContext.getInputCount(); i++)
+	{
+		if(l_rDynamicBoxContext.getInputChunkCount(i)==0)
+		{
+			return true;
+		}
+		if(l_ui64StartTime!=l_rDynamicBoxContext.getInputChunkStartTime(i, 0)) { return false; }
+		if(l_ui64EndTime!=l_rDynamicBoxContext.getInputChunkEndTime(i, 0)) { return false; }
+	}
+
+	this->getBoxAlgorithmContext()->markAlgorithmAsReadyToProcess();
 	return true;
 }
 
 boolean CBoxAlgorithmSignalMerger::process(void)
 {
 	IBox& l_rStaticBoxContext=this->getStaticBoxContext();
-	IBoxIO* l_pDynamicBoxContext=getBoxAlgorithmContext()->getDynamicBoxContext();
+	IBoxIO& l_rDynamicBoxContext=this->getDynamicBoxContext();
 
 	uint32 i, j, k;
-	uint32 l_ui32MinimumChunkInputCount=l_pDynamicBoxContext->getInputChunkCount(0);
-	for(i=1; i<l_rStaticBoxContext.getInputCount(); i++)
+	uint32 l_ui32SampleCountPerSentBlock=0;
+	uint32 l_ui32ChannelCount=0;
+	uint32 l_ui32HeaderCount=0;
+	uint32 l_ui32BufferCount=0;
+	uint32 l_ui32EndCount=0;
+
+	for(i=0; i<l_rStaticBoxContext.getInputCount(); i++)
 	{
-		l_ui32MinimumChunkInputCount=std::min(l_pDynamicBoxContext->getInputChunkCount(i),l_ui32MinimumChunkInputCount);
+		TParameterHandler < const IMemoryBuffer* > ip_pMemoryBuffer(m_vStreamDecoder[i]->getInputParameter(OVP_GD_Algorithm_SignalStreamDecoder_InputParameterId_MemoryBufferToDecode));
+		TParameterHandler < IMatrix* > op_pMatrix(m_vStreamDecoder[i]->getOutputParameter(OVP_GD_Algorithm_SignalStreamDecoder_OutputParameterId_Matrix));
+
+		ip_pMemoryBuffer=l_rDynamicBoxContext.getInputChunk(i, 0);
+
+		m_vStreamDecoder[i]->process();
+		if(m_vStreamDecoder[i]->isOutputTriggerActive(OVP_GD_Algorithm_SignalStreamDecoder_OutputTriggerId_ReceivedHeader))
+		{
+			l_ui32HeaderCount++;
+			if(i==0)
+			{
+				l_ui32SampleCountPerSentBlock=op_pMatrix->getDimensionSize(1);
+				l_ui32ChannelCount=op_pMatrix->getDimensionSize(0);
+			}
+			else
+			{
+				if(l_ui32SampleCountPerSentBlock!=op_pMatrix->getDimensionSize(1)) { return false; }
+				l_ui32ChannelCount+=op_pMatrix->getDimensionSize(0);
+			}
+		}
+		if(m_vStreamDecoder[i]->isOutputTriggerActive(OVP_GD_Algorithm_SignalStreamDecoder_OutputTriggerId_ReceivedBuffer))
+		{
+			l_ui32BufferCount++;
+		}
+		if(m_vStreamDecoder[i]->isOutputTriggerActive(OVP_GD_Algorithm_SignalStreamDecoder_OutputTriggerId_ReceivedEnd))
+		{
+			l_ui32EndCount++;
+		}
+
+		l_rDynamicBoxContext.markInputAsDeprecated(i, 0);
 	}
 
-	for(j=0; j<l_ui32MinimumChunkInputCount; j++)
+	if(l_ui32HeaderCount && l_ui32HeaderCount!=l_rStaticBoxContext.getInputCount()) { return false; }
+	if(l_ui32BufferCount && l_ui32BufferCount!=l_rStaticBoxContext.getInputCount()) { return false; }
+	if(l_ui32EndCount && l_ui32EndCount!=l_rStaticBoxContext.getInputCount()) { return false; }
+
+	TParameterHandler < IMatrix* > ip_pMatrix(m_pStreamEncoder->getInputParameter(OVP_GD_Algorithm_SignalStreamEncoder_InputParameterId_Matrix));
+	TParameterHandler < IMemoryBuffer* > op_pMemoryBuffer(m_pStreamEncoder->getOutputParameter(OVP_GD_Algorithm_SignalStreamEncoder_OutputParameterId_EncodedMemoryBuffer));
+	op_pMemoryBuffer=l_rDynamicBoxContext.getOutputChunk(0);
+
+	if(l_ui32HeaderCount)
 	{
-		uint32 l_ui32DecoderType=DecoderType_Unknown;
-		uint64 l_ui64SamplingRate=uint64(-1);
-		uint32 l_ui32ChannelCount=0;
-		uint32 l_ui32SampleCount=0;
-		uint64 l_ui64IndexCount=0;
-		uint64 l_ui64StartTime=l_pDynamicBoxContext->getInputChunkStartTime(0, j);
-		uint64 l_ui64EndTime=l_pDynamicBoxContext->getInputChunkEndTime(0, j);
-
-		op_pEncodedMemoryBuffer=l_pDynamicBoxContext->getOutputChunk(0);
-
+		k=0;
+		ip_pMatrix->setDimensionCount(2);
+		ip_pMatrix->setDimensionSize(0, l_ui32ChannelCount);
+		ip_pMatrix->setDimensionSize(1, l_ui32SampleCountPerSentBlock);
 		for(i=0; i<l_rStaticBoxContext.getInputCount(); i++)
 		{
-			// décodage
-			ip_pMemoryBufferToDecode1=l_pDynamicBoxContext->getInputChunk(i, j);
-			m_pSignalDecoder->process();
-
-			// uniformisation
-			if(l_ui32DecoderType!=DecoderType_Error)
+			TParameterHandler < IMatrix* > op_pMatrix(m_vStreamDecoder[i]->getOutputParameter(OVP_GD_Algorithm_SignalStreamDecoder_OutputParameterId_Matrix));
+			for(j=0; j<op_pMatrix->getDimensionSize(0); j++, k++)
 			{
-				if(m_pSignalDecoder->isOutputTriggerActive(OVP_GD_Algorithm_SignalStreamDecoder_OutputTriggerId_ReceivedHeader))
-				{
-					l_ui32DecoderType=(l_ui32DecoderType==DecoderType_Unknown ? DecoderType_Header : (l_ui32DecoderType!=DecoderType_Header ? DecoderType_Error : l_ui32DecoderType));
-				}
-				if(m_pSignalDecoder->isOutputTriggerActive(OVP_GD_Algorithm_SignalStreamDecoder_OutputTriggerId_ReceivedBuffer))
-				{
-					l_ui32DecoderType=(l_ui32DecoderType==DecoderType_Unknown ? DecoderType_Buffer : (l_ui32DecoderType!=DecoderType_Buffer ? DecoderType_Error : l_ui32DecoderType));
-				}
-				if(m_pSignalDecoder->isOutputTriggerActive(OVP_GD_Algorithm_SignalStreamDecoder_OutputTriggerId_ReceivedEnd))
-				{
-					l_ui32DecoderType=(l_ui32DecoderType==DecoderType_Unknown ? DecoderType_End : (l_ui32DecoderType!=DecoderType_End ? DecoderType_Error : l_ui32DecoderType));
-				}
+				ip_pMatrix->setDimensionLabel(0, k, op_pMatrix->getDimensionLabel(0, j));
 			}
-
-			// action
-			if(l_ui32DecoderType==DecoderType_Header)
-			{
-				// Test of entry flux sampling rate coherency
-				if(l_ui64SamplingRate==uint64(-1))
-				{
-					l_ui64SamplingRate=op_ui64SamplingRate1;
-				}
-				if(l_ui64SamplingRate!=op_ui64SamplingRate1)
-				{
-					this->getLogManager() << LogLevel_Error << "Sampling rate mismatch : got " << l_ui64SamplingRate << "Hz - wanted " << op_ui64SamplingRate1 << "Hz.\n";
-					return false;
-				}
-
-				// Test chunk size coherency
-				if(l_ui32SampleCount==0)
-				{
-					l_ui32SampleCount=op_pDecodedMatrix1->getDimensionSize(1);
-				}
-				if(l_ui32SampleCount!=op_pDecodedMatrix1->getDimensionSize(1))
-				{
-					this->getLogManager() << LogLevel_Error << "Epoch size mismatch : got " << l_ui32SampleCount << " samples - wanted " << op_pDecodedMatrix1->getDimensionSize(1) << " samples.\n";
-					return false;
-				}
-
-				// Memory reservation
-				m_vChannelCount.push_back(op_pDecodedMatrix1->getDimensionSize(0));
-				l_ui32ChannelCount+=op_pDecodedMatrix1->getDimensionSize(0);
-
-				// Channel names
-				for(k=0; k<op_pDecodedMatrix1->getDimensionSize(0); k++)
-				{
-					m_vChannelName.push_back(op_pDecodedMatrix1->getDimensionLabel(0, k));
-				}
-
-				// send data
-				if(i==l_rStaticBoxContext.getInputCount()-1)
-				{
-					ip_pMatrixToEncode->setDimensionCount(2);
-					ip_pMatrixToEncode->setDimensionSize(0, l_ui32ChannelCount);
-					ip_pMatrixToEncode->setDimensionSize(1, l_ui32SampleCount);
-
-					for(k=0; k<l_ui32ChannelCount; k++)
-					{
-						ip_pMatrixToEncode->setDimensionLabel(0, k, m_vChannelName[k].c_str());
-					}
-
-					m_pSignalEncoder->process(OVP_GD_Algorithm_SignalStreamEncoder_InputTriggerId_EncodeHeader);
-					l_pDynamicBoxContext->markOutputAsReadyToSend(0, l_ui64StartTime, l_ui64EndTime);
-				}
-			}
-
-			if(l_ui32DecoderType==DecoderType_Buffer)
-			{
-				if( (l_ui64StartTime !=0 && l_ui64StartTime!=l_pDynamicBoxContext->getInputChunkStartTime(i, j))
-				    || (l_ui64EndTime!=0 && l_ui64EndTime!=l_pDynamicBoxContext->getInputChunkEndTime(i, j)) )
-				{
-					this->getLogManager() << LogLevel_Warning << "Chunk time mismatch : ["
-						<< l_pDynamicBoxContext->getInputChunkStartTime(i, j) << " | " << l_pDynamicBoxContext->getInputChunkEndTime(i, j)
-						<< "] - wanted [" << l_ui64StartTime << " | " << l_ui64EndTime << "]\n";
-					return false;
-				}
-
-				for(k=0; k<m_vChannelCount.at(i)*op_pDecodedMatrix1->getDimensionSize(1); k++)
-				{
-					ip_pMatrixToEncode->getBuffer()[l_ui64IndexCount+k]=op_pDecodedMatrix1->getBuffer()[k];
-				}
-				l_ui64IndexCount+=m_vChannelCount.at(i)*op_pDecodedMatrix1->getDimensionSize(1);
-				if(l_ui64IndexCount>ip_pMatrixToEncode->getDimensionSize(0)*ip_pMatrixToEncode->getDimensionSize(1))
-				{
-					// This should never happen
-					this->getLogManager() << LogLevel_Warning << "Buffer overflow : " << l_ui64IndexCount << " / " << ip_pMatrixToEncode->getDimensionSize(0)*ip_pMatrixToEncode->getDimensionSize(1) << "\n";
-					return false;
-				}
-
-				if(i==l_rStaticBoxContext.getInputCount()-1)
-				{
-					m_pSignalEncoder->process(OVP_GD_Algorithm_SignalStreamEncoder_InputTriggerId_EncodeBuffer);
-					l_pDynamicBoxContext->markOutputAsReadyToSend(0, l_pDynamicBoxContext->getInputChunkStartTime(i, j), l_pDynamicBoxContext->getInputChunkEndTime(i, j));
-				}
-			}
-
-			if(l_ui32DecoderType==DecoderType_End)
-			{
-				if(i==l_rStaticBoxContext.getInputCount()-1)
-				{
-					m_pSignalEncoder->process(OVP_GD_Algorithm_SignalStreamEncoder_InputTriggerId_EncodeEnd);
-					l_pDynamicBoxContext->markOutputAsReadyToSend(0, l_pDynamicBoxContext->getInputChunkStartTime(i, j), l_pDynamicBoxContext->getInputChunkEndTime(i, j));
-				}
-			}
-
-			l_pDynamicBoxContext->markInputAsDeprecated(i, j);
 		}
+		m_pStreamEncoder->process(OVP_GD_Algorithm_SignalStreamEncoder_InputTriggerId_EncodeHeader);
+	}
+
+	l_ui32SampleCountPerSentBlock=ip_pMatrix->getDimensionSize(1);
+
+	if(l_ui32BufferCount)
+	{
+		k=0;
+		for(i=0; i<l_rStaticBoxContext.getInputCount(); i++)
+		{
+			TParameterHandler < IMatrix* > op_pMatrix(m_vStreamDecoder[i]->getOutputParameter(OVP_GD_Algorithm_SignalStreamDecoder_OutputParameterId_Matrix));
+			for(j=0; j<op_pMatrix->getDimensionSize(0); j++, k++)
+			{
+				System::Memory::copy(ip_pMatrix->getBuffer() + k*l_ui32SampleCountPerSentBlock, op_pMatrix->getBuffer() + j*l_ui32SampleCountPerSentBlock, l_ui32SampleCountPerSentBlock*sizeof(float64));
+			}
+		}
+		m_pStreamEncoder->process(OVP_GD_Algorithm_SignalStreamEncoder_InputTriggerId_EncodeBuffer);
+	}
+
+	if(l_ui32EndCount)
+	{
+		m_pStreamEncoder->process(OVP_GD_Algorithm_SignalStreamEncoder_InputTriggerId_EncodeEnd);
+	}
+
+	if(l_ui32HeaderCount!=0 || l_ui32BufferCount!=0 || l_ui32EndCount!=0)
+	{
+		l_rDynamicBoxContext.markOutputAsReadyToSend(0, l_rDynamicBoxContext.getInputChunkStartTime(0, 0), l_rDynamicBoxContext.getInputChunkEndTime(0, 0));
 	}
 
 	return true;
--- trunk/openvibe-plugins/streaming/trunc/src/box-algorithms/streaming/ovpCBoxAlgorithmSignalMerger.h
+++ trunk/openvibe-plugins/streaming/trunc/src/box-algorithms/streaming/ovpCBoxAlgorithmSignalMerger.h
@@ -4,13 +4,11 @@
 #include "../../ovp_defines.h"
 #include <openvibe/ov_all.h>
 #include <openvibe-toolkit/ovtk_all.h>
-
 #include <vector>
 #include <cstdio>
-#include <string>
 
-#define OVP_ClassId_BoxAlgorithm_SignalMerger 	 	OpenViBE::CIdentifier(0x4BF9326F, 0x75603102)
-#define OVP_ClassId_BoxAlgorithm_SignalMergerDesc 	OpenViBE::CIdentifier(0x7A684C44, 0x23BA70A5)
+#define OVP_ClassId_BoxAlgorithm_SignalMerger     OpenViBE::CIdentifier(0x4BF9326F, 0x75603102)
+#define OVP_ClassId_BoxAlgorithm_SignalMergerDesc OpenViBE::CIdentifier(0x7A684C44, 0x23BA70A5)
 
 namespace OpenViBEPlugins
 {
@@ -31,38 +29,46 @@ namespace OpenViBEPlugins
 
 		protected:
 
-			OpenViBE::Kernel::IAlgorithmProxy* m_pSignalDecoder;
-			OpenViBE::Kernel::TParameterHandler < const OpenViBE::IMemoryBuffer* > ip_pMemoryBufferToDecode1;
-            OpenViBE::Kernel::TParameterHandler < OpenViBE::IMatrix* > op_pDecodedMatrix1;
-			OpenViBE::Kernel::TParameterHandler < OpenViBE::uint64 > op_ui64SamplingRate1;
-
-			OpenViBE::Kernel::IAlgorithmProxy* m_pSignalEncoder;
-			OpenViBE::Kernel::TParameterHandler < OpenViBE::IMatrix* > ip_pMatrixToEncode;
-			OpenViBE::Kernel::TParameterHandler < OpenViBE::IMemoryBuffer* > op_pEncodedMemoryBuffer;
-			OpenViBE::Kernel::TParameterHandler < OpenViBE::uint64 > ip_ui64SamplingRate;
-
-			std::vector < OpenViBE::uint32> m_vChannelCount;
-			std::vector < std::string > m_vChannelName;
+			std::vector < OpenViBE::Kernel::IAlgorithmProxy* > m_vStreamDecoder;
+			OpenViBE::Kernel::IAlgorithmProxy* m_pStreamEncoder;
 		};
 
 		class CBoxAlgorithmSignalMergerListener : public OpenViBEToolkit::TBoxListener < OpenViBE::Plugins::IBoxListener >
 		{
 		public:
 
+//			virtual OpenViBE::boolean initialize(void) { return true; }
+//			virtual OpenViBE::boolean uninitialize(void) { return true; }
+//			virtual OpenViBE::boolean onInitialized(OpenViBE::Kernel::IBox& rBox) { return true; };
+//			virtual OpenViBE::boolean onNameChanged(OpenViBE::Kernel::IBox& rBox) { return true; };
+//			virtual OpenViBE::boolean onIdentifierChanged(OpenViBE::Kernel::IBox& rBox) { return true; };
+//			virtual OpenViBE::boolean onAlgorithmClassIdentifierChanged(OpenViBE::Kernel::IBox& rBox) { return true; };
+//			virtual OpenViBE::boolean onProcessingUnitChanged(OpenViBE::Kernel::IBox& rBox) { return true; };
+//			virtual OpenViBE::boolean onInputConnected(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onInputDisconnected(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
 			virtual OpenViBE::boolean onInputAdded(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index)
 			{
-				char l_sName[1024];
-				OpenViBE::uint32 i;
-
+				char l_sInputName[1024];
+				::sprintf(l_sInputName, "Input %i", ui32Index);
+				rBox.setInputName(ui32Index, l_sInputName);
 				rBox.setInputType(ui32Index, OV_TypeId_Signal);
-				for(i=0; i<rBox.getInputCount(); i++)
-				{
-					sprintf(l_sName, "Input Signal %u", i+1);
-					rBox.setInputName(i, l_sName);
-				}
-
 				return true;
-			};
+			}
+//			virtual OpenViBE::boolean onInputRemoved(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onInputTypeChanged(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onInputNameChanged(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onOutputConnected(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onOutputDisconnected(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onOutputAdded(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onOutputRemoved(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onOutputTypeChanged(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onOutputNameChanged(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onSettingAdded(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onSettingRemoved(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onSettingTypeChanged(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onSettingNameChanged(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onSettingDefaultValueChanged(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
+//			virtual OpenViBE::boolean onSettingValueChanged(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index) { return true; };
 
 			_IsDerivedFromClass_Final_(OpenViBEToolkit::TBoxListener < OpenViBE::Plugins::IBoxListener >, OV_UndefinedIdentifier);
 		};
@@ -74,10 +80,10 @@ namespace OpenViBEPlugins
 			virtual void release(void) { }
 
 			virtual OpenViBE::CString getName(void) const                { return OpenViBE::CString("Signal Merger"); }
-			virtual OpenViBE::CString getAuthorName(void) const          { return OpenViBE::CString("Guillaume Lio - Matthieu Goyat"); }
-			virtual OpenViBE::CString getAuthorCompanyName(void) const   { return OpenViBE::CString("GIPSA-Lab"); }
-			virtual OpenViBE::CString getShortDescription(void) const    { return OpenViBE::CString("Channels concatenation of two signals."); }
-			virtual OpenViBE::CString getDetailedDescription(void) const { return OpenViBE::CString("Converts some signal in one signal through channel addition. Signals must have the same epoch and the same sampling rate."); }
+			virtual OpenViBE::CString getAuthorName(void) const          { return OpenViBE::CString("Yann Renard"); }
+			virtual OpenViBE::CString getAuthorCompanyName(void) const   { return OpenViBE::CString("INRIA"); }
+			virtual OpenViBE::CString getShortDescription(void) const    { return OpenViBE::CString("..."); }
+			virtual OpenViBE::CString getDetailedDescription(void) const { return OpenViBE::CString("..."); }
 			virtual OpenViBE::CString getCategory(void) const            { return OpenViBE::CString("Streaming"); }
 			virtual OpenViBE::CString getVersion(void) const             { return OpenViBE::CString("1.0"); }
 			virtual OpenViBE::CString getStockItemName(void) const       { return OpenViBE::CString("gtk-sort-ascending"); }
@@ -90,9 +96,9 @@ namespace OpenViBEPlugins
 			virtual OpenViBE::boolean getBoxPrototype(
 				OpenViBE::Kernel::IBoxProto& rBoxAlgorithmPrototype) const
 			{
-				rBoxAlgorithmPrototype.addInput  ("Input Signal 1", OV_TypeId_Signal);
-				rBoxAlgorithmPrototype.addInput  ("Input Signal 2", OV_TypeId_Signal);
-				rBoxAlgorithmPrototype.addOutput ("Merged Signal", OV_TypeId_Signal);
+				rBoxAlgorithmPrototype.addInput  ("Input 1", OV_TypeId_Signal);
+				rBoxAlgorithmPrototype.addInput  ("Input 2", OV_TypeId_Signal);
+				rBoxAlgorithmPrototype.addOutput ("Merged", OV_TypeId_Signal);
 				rBoxAlgorithmPrototype.addFlag   (OpenViBE::Kernel::BoxFlag_CanAddInput);
 
 				return true;
