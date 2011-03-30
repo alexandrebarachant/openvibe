e8af6f29988cdb2e46dcd4593d188013f7968401
Jozef Legeny <dev@openvibe.inria.fr>
2011-03-30 15:28:53 +0000

    openvibe-applications/ssvep: + added an external application for SSVEP paradigm + added a 'trainer' application usable for classifier training and online experiments + added a 'shooter' application for online SSVEP testing
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@2852 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

---
+++ trunk/openvibe-applications/ssvep-demo/trunc/src/ovassvepCSSVEPFlickeringObject.cpp
@@ -0,0 +1,34 @@
+#include "ovassvepCSSVEPFlickeringObject.h"
+#include <iostream>
+
+using namespace OpenViBESSVEP;
+
+CSSVEPFlickeringObject::CSSVEPFlickeringObject(Ogre::SceneNode* poObjectNode, OpenViBE::uint32 ui32LitFrames, OpenViBE::uint32 ui32DarkFrames) :
+	m_poObjectNode( poObjectNode ),
+	m_ui32LitFrames( ui32LitFrames ),
+	m_ui32DarkFrames( ui32DarkFrames ),
+	m_bVisible( true )
+{
+	std::cout << "lit " << ui32LitFrames << " dark " << ui32DarkFrames << "\n";
+}
+
+void CSSVEPFlickeringObject::setVisible( bool bVisibility )
+{
+	if ( (!m_bVisible && bVisibility) || (m_bVisible && !bVisibility) )
+	{
+		m_poObjectNode->flipVisibility();
+	}
+	m_bVisible = bVisibility;
+}
+
+void CSSVEPFlickeringObject::processFrame(OpenViBE::uint32 ui32CurrentFrame)
+{
+	if (ui32CurrentFrame % ( m_ui32LitFrames + m_ui32DarkFrames ) < m_ui32LitFrames)
+	{
+		this->setVisible( true );
+	}
+	else
+	{
+		this->setVisible( false );
+	}
+}
