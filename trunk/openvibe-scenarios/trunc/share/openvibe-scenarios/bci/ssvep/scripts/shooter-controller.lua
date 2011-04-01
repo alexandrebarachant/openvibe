a0c46ee3244a03e76428119d29916b639fae995e
Jozef Legeny <dev@openvibe.inria.fr>
2011-04-01 16:55:24 +0000

    openvibe-scenarios/bci/ssvep: * updated scenario comments * modified the shooter lua script to take speed setting into account
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@2870 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/openvibe-scenarios/trunc/share/openvibe-scenarios/bci/ssvep/scripts/shooter-controller.lua
+++ trunk/openvibe-scenarios/trunc/share/openvibe-scenarios/bci/ssvep/scripts/shooter-controller.lua
@@ -4,12 +4,25 @@ flickeringDelay = nil
 
 function initialize(box)
 	s_sequence = box:get_setting(2)
+	angular_speed = box:get_setting(3)
 
-	io.write(string.format("Target Sequence : [%s]\n", s_sequence))
+	box:log("Info", string.format("Target Sequence : [%s]\n", s_sequence))
 
 	for target in s_sequence:gmatch("%d+") do
 		table.insert(sequence, target)
 	end
+
+	box:log("Info", string.format("Angular Speed : %g\n", angular_speed))
+	
+	-- create the configuration file for the shooter application
+	cfg_file = io.open(box:get_config("${CustomConfigurationPrefix${OperatingSystem}}-ssvep-demo-shooter${CustomConfigurationSuffix${OperatingSystem}}"), "w")
+
+	cfg_file:write("# This file was automatically generated!\n\n")
+	cfg_file:write("# If you want to change the SSVEP online shooter configuration\n")
+	cfg_file:write("# please use the box settings in the shooter scenario.\n\n")
+
+	cfg_file:write("SSVEP_ShipAngularSpeed = ", angular_speed, "\n")
+	
 
 end
 
