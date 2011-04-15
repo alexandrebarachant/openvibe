9f281ec453b0b63404aa77e2783185d768cd06c9
Laurent Bonnet <dev@openvibe.inria.fr>
2011-04-15 12:00:08 +0000

    openvibe-designer: * ovdCInterfacedScenario.cpp : Added shortcuts for the player control : F5=stop, F6=next-step, F7=play/pause, F8=fastforward (same order as in the toolbar)
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@2922 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/openvibe-applications/designer/trunc/src/ovdCInterfacedScenario.cpp
+++ trunk/openvibe-applications/designer/trunc/src/ovdCInterfacedScenario.cpp
@@ -1948,6 +1948,36 @@ void CInterfacedScenario::scenarioDrawingAreaKeyPressEventCB(::GtkWidget* pWidge
 		contextMenuBoxRenameAllCB();
 	}
 
+	//The shortcuts respect the order in the toolbar
+
+	// F7 :play/pause
+	if(pEvent->keyval==GDK_F7)
+	{
+		if(m_rApplication.getCurrentInterfacedScenario()->m_ePlayerStatus == PlayerStatus_Play)
+		{
+			m_rApplication.pauseScenarioCB();
+		}
+		else
+		{
+			m_rApplication.playScenarioCB();
+		}
+	}
+	// F6 : step
+	if(pEvent->keyval==GDK_F6)
+	{
+		m_rApplication.nextScenarioCB();
+	}
+	// F8 :fastforward
+	if(pEvent->keyval==GDK_F8)
+	{
+		m_rApplication.forwardScenarioCB();
+	}
+	// F5 : stop
+	if(pEvent->keyval==GDK_F5)
+	{
+		m_rApplication.stopScenarioCB();
+	}
+
 	m_rKernelContext.getLogManager() << LogLevel_Debug
 		<< "scenarioDrawingAreaKeyPressEventCB ("
 		<< (m_bShiftPressed?"true":"false") << "|"
