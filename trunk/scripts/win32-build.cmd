5918942efdadd2a01191feb84a33e07c9221693f
Yann Renard <dev@openvibe.inria.fr>
2011-04-21 16:34:55 +0000

    scripts :  * for convenience & Windows / Linux homogeneity, renamed launchers from test-* to ov-* on Windows
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@2954 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/scripts/win32-build.cmd
+++ trunk/scripts/win32-build.cmd
@@ -86,88 +86,87 @@ mkdir %target_dist%\doc           > NULL 2<&1
 mkdir %target_dist%\log           > NULL 2<&1
 mkdir %target_dist%\tmp           > NULL 2<&1
 
-echo @echo off                                                            >  %target_dist%\test-vr-demo-tie-fighter.cmd
-echo SET OpenViBE_DistRoot=%%CD%%>> %target_dist%\test-vr-demo-tie-fighter.cmd
-echo pushd ..\scripts                                                     >> %target_dist%\test-vr-demo-tie-fighter.cmd
-echo call win32-init_env_command.cmd                                      >> %target_dist%\test-vr-demo-tie-fighter.cmd
-echo popd                                                                 >> %target_dist%\test-vr-demo-tie-fighter.cmd
-echo copy share\openvibe-applications\vr-demo\tie-fighter\resources.cfg-base share\openvibe-applications\vr-demo\tie-fighter\resources.cfg >> %target_dist%\test-vr-demo-tie-fighter.cmd
-echo type %OV_DEP_CEGUI%\resources.cfg ^>^> share\openvibe-applications\vr-demo\tie-fighter\resources.cfg >> %target_dist%\test-vr-demo-tie-fighter.cmd
-echo cd share\openvibe-applications\vr-demo\tie-fighter                   >> %target_dist%\test-vr-demo-tie-fighter.cmd
-echo %%OpenViBE_DistRoot%%\bin\OpenViBE-vr-demo-dynamic.exe tie-fighter   >> %target_dist%\test-vr-demo-tie-fighter.cmd
-echo cd %%OpenViBE_DistRoot%%                                             >> %target_dist%\test-vr-demo-tie-fighter.cmd
-echo exit                                                                 >> %target_dist%\test-vr-demo-tie-fighter.cmd
+echo @echo off                                                            >  %target_dist%\ov-vr-demo-tie-fighter.cmd
+echo SET OpenViBE_DistRoot=%%CD%%>> %target_dist%\ov-vr-demo-tie-fighter.cmd
+echo pushd ..\scripts                                                     >> %target_dist%\ov-vr-demo-tie-fighter.cmd
+echo call win32-init_env_command.cmd                                      >> %target_dist%\ov-vr-demo-tie-fighter.cmd
+echo popd                                                                 >> %target_dist%\ov-vr-demo-tie-fighter.cmd
+echo copy share\openvibe-applications\vr-demo\tie-fighter\resources.cfg-base share\openvibe-applications\vr-demo\tie-fighter\resources.cfg >> %target_dist%\ov-vr-demo-tie-fighter.cmd
+echo type %OV_DEP_CEGUI%\resources.cfg ^>^> share\openvibe-applications\vr-demo\tie-fighter\resources.cfg >> %target_dist%\ov-vr-demo-tie-fighter.cmd
+echo cd share\openvibe-applications\vr-demo\tie-fighter                   >> %target_dist%\ov-vr-demo-tie-fighter.cmd
+echo %%OpenViBE_DistRoot%%\bin\OpenViBE-vr-demo-dynamic.exe tie-fighter   >> %target_dist%\ov-vr-demo-tie-fighter.cmd
+echo cd %%OpenViBE_DistRoot%%                                             >> %target_dist%\ov-vr-demo-tie-fighter.cmd
+echo exit                                                                 >> %target_dist%\ov-vr-demo-tie-fighter.cmd
 
-echo @echo off                                                            >  %target_dist%\test-vr-demo-handball.cmd
-echo SET OpenViBE_DistRoot=%%CD%%>> %target_dist%\test-vr-demo-handball.cmd
-echo pushd ..\scripts                                                     >> %target_dist%\test-vr-demo-handball.cmd
-echo call win32-init_env_command.cmd                                      >> %target_dist%\test-vr-demo-handball.cmd
-echo popd                                                                 >> %target_dist%\test-vr-demo-handball.cmd
-echo copy share\openvibe-applications\vr-demo\handball\resources.cfg-base share\openvibe-applications\vr-demo\handball\resources.cfg >> %target_dist%\test-vr-demo-handball.cmd
-echo type %OV_DEP_CEGUI%\resources.cfg ^>^> share\openvibe-applications\vr-demo\handball\resources.cfg >> %target_dist%\test-vr-demo-handball.cmd
-echo cd share\openvibe-applications\vr-demo\handball                      >> %target_dist%\test-vr-demo-handball.cmd
-echo %%OpenViBE_DistRoot%%\bin\OpenViBE-vr-demo-dynamic.exe handball      >> %target_dist%\test-vr-demo-handball.cmd
-echo cd %%OpenViBE_DistRoot%%                                             >> %target_dist%\test-vr-demo-handball.cmd
-echo exit                                                                 >> %target_dist%\test-vr-demo-handball.cmd
+echo @echo off                                                            >  %target_dist%\ov-vr-demo-handball.cmd
+echo SET OpenViBE_DistRoot=%%CD%%>> %target_dist%\ov-vr-demo-handball.cmd
+echo pushd ..\scripts                                                     >> %target_dist%\ov-vr-demo-handball.cmd
+echo call win32-init_env_command.cmd                                      >> %target_dist%\ov-vr-demo-handball.cmd
+echo popd                                                                 >> %target_dist%\ov-vr-demo-handball.cmd
+echo copy share\openvibe-applications\vr-demo\handball\resources.cfg-base share\openvibe-applications\vr-demo\handball\resources.cfg >> %target_dist%\ov-vr-demo-handball.cmd
+echo type %OV_DEP_CEGUI%\resources.cfg ^>^> share\openvibe-applications\vr-demo\handball\resources.cfg >> %target_dist%\ov-vr-demo-handball.cmd
+echo cd share\openvibe-applications\vr-demo\handball                      >> %target_dist%\ov-vr-demo-handball.cmd
+echo %%OpenViBE_DistRoot%%\bin\OpenViBE-vr-demo-dynamic.exe handball      >> %target_dist%\ov-vr-demo-handball.cmd
+echo cd %%OpenViBE_DistRoot%%                                             >> %target_dist%\ov-vr-demo-handball.cmd
+echo exit                                                                 >> %target_dist%\ov-vr-demo-handball.cmd
 
-echo @echo off                                               >  %target_dist%\test-acquisition-server.cmd
-echo pushd ..\scripts                                        >> %target_dist%\test-acquisition-server.cmd
-echo call win32-init_env_command.cmd                         >> %target_dist%\test-acquisition-server.cmd
-echo popd                                                    >> %target_dist%\test-acquisition-server.cmd
-echo cd bin                                                  >> %target_dist%\test-acquisition-server.cmd
-echo OpenViBE-acquisition-server-dynamic.exe %%1 %%2 %%3 %%4 >> %target_dist%\test-acquisition-server.cmd
-echo pause                                                   >> %target_dist%\test-acquisition-server.cmd
+echo @echo off                                               >  %target_dist%\ov-acquisition-server.cmd
+echo pushd ..\scripts                                        >> %target_dist%\ov-acquisition-server.cmd
+echo call win32-init_env_command.cmd                         >> %target_dist%\ov-acquisition-server.cmd
+echo popd                                                    >> %target_dist%\ov-acquisition-server.cmd
+echo cd bin                                                  >> %target_dist%\ov-acquisition-server.cmd
+echo OpenViBE-acquisition-server-dynamic.exe %%1 %%2 %%3 %%4 >> %target_dist%\ov-acquisition-server.cmd
+echo pause                                                   >> %target_dist%\ov-acquisition-server.cmd
 
-echo @echo off                                               >  %target_dist%\test-designer.cmd
-echo pushd ..\scripts                                        >> %target_dist%\test-designer.cmd
-echo call win32-init_env_command.cmd                         >> %target_dist%\test-designer.cmd
-echo popd                                                    >> %target_dist%\test-designer.cmd
-echo cd bin                                                  >> %target_dist%\test-designer.cmd
-echo OpenViBE-designer-dynamic.exe %%1 %%2 %%3 %%4 %%5 %%6   >> %target_dist%\test-designer.cmd
-echo pause                                                   >> %target_dist%\test-designer.cmd
+echo @echo off                                               >  %target_dist%\ov-designer.cmd
+echo pushd ..\scripts                                        >> %target_dist%\ov-designer.cmd
+echo call win32-init_env_command.cmd                         >> %target_dist%\ov-designer.cmd
+echo popd                                                    >> %target_dist%\ov-designer.cmd
+echo cd bin                                                  >> %target_dist%\ov-designer.cmd
+echo OpenViBE-designer-dynamic.exe %%1 %%2 %%3 %%4 %%5 %%6   >> %target_dist%\ov-designer.cmd
+echo pause                                                   >> %target_dist%\ov-designer.cmd
 
-echo @echo off                                               >  %target_dist%\test-id-generator.cmd
-echo pushd ..\scripts                                        >> %target_dist%\test-id-generator.cmd
-echo call win32-init_env_command.cmd                         >> %target_dist%\test-id-generator.cmd
-echo popd                                                    >> %target_dist%\test-id-generator.cmd
-echo cd bin                                                  >> %target_dist%\test-id-generator.cmd
-echo OpenViBE-id-generator-dynamic.exe %%1 %%2 %%3 %%4 %%5   >> %target_dist%\test-id-generator.cmd
-echo pause                                                   >> %target_dist%\test-id-generator.cmd
+echo @echo off                                               >  %target_dist%\ov-id-generator.cmd
+echo pushd ..\scripts                                        >> %target_dist%\ov-id-generator.cmd
+echo call win32-init_env_command.cmd                         >> %target_dist%\ov-id-generator.cmd
+echo popd                                                    >> %target_dist%\ov-id-generator.cmd
+echo cd bin                                                  >> %target_dist%\ov-id-generator.cmd
+echo OpenViBE-id-generator-dynamic.exe %%1 %%2 %%3 %%4 %%5   >> %target_dist%\ov-id-generator.cmd
+echo pause                                                   >> %target_dist%\ov-id-generator.cmd
 
-echo @echo off                                               >  %target_dist%\test-plugin-inspector.cmd
-echo pushd ..\scripts                                        >> %target_dist%\test-plugin-inspector.cmd
-echo call win32-init_env_command.cmd                         >> %target_dist%\test-plugin-inspector.cmd
-echo popd                                                    >> %target_dist%\test-plugin-inspector.cmd
-echo cd bin                                                  >> %target_dist%\test-plugin-inspector.cmd
-echo OpenViBE-plugin-inspector-dynamic.exe %%1 %%2 %%3 %%4   >> %target_dist%\test-plugin-inspector.cmd
-echo pause                                                   >> %target_dist%\test-plugin-inspector.cmd
+echo @echo off                                               >  %target_dist%\ov-plugin-inspector.cmd
+echo pushd ..\scripts                                        >> %target_dist%\ov-plugin-inspector.cmd
+echo call win32-init_env_command.cmd                         >> %target_dist%\ov-plugin-inspector.cmd
+echo popd                                                    >> %target_dist%\ov-plugin-inspector.cmd
+echo cd bin                                                  >> %target_dist%\ov-plugin-inspector.cmd
+echo OpenViBE-plugin-inspector-dynamic.exe %%1 %%2 %%3 %%4   >> %target_dist%\ov-plugin-inspector.cmd
+echo pause                                                   >> %target_dist%\ov-plugin-inspector.cmd
 
-echo @echo off                                               >  %target_dist%\test-skeleton-generator.cmd
-echo pushd ..\scripts                                        >> %target_dist%\test-skeleton-generator.cmd
-echo call win32-init_env_command.cmd                         >> %target_dist%\test-skeleton-generator.cmd
-echo popd                                                    >> %target_dist%\test-skeleton-generator.cmd
-echo cd bin                                                  >> %target_dist%\test-skeleton-generator.cmd
-echo OpenViBE-skeleton-generator-dynamic.exe %%1 %%2 %%3 %%4 >> %target_dist%\test-skeleton-generator.cmd
-echo pause                                                   >> %target_dist%\test-skeleton-generator.cmd
+echo @echo off                                               >  %target_dist%\ov-skeleton-generator.cmd
+echo pushd ..\scripts                                        >> %target_dist%\ov-skeleton-generator.cmd
+echo call win32-init_env_command.cmd                         >> %target_dist%\ov-skeleton-generator.cmd
+echo popd                                                    >> %target_dist%\ov-skeleton-generator.cmd
+echo cd bin                                                  >> %target_dist%\ov-skeleton-generator.cmd
+echo OpenViBE-skeleton-generator-dynamic.exe %%1 %%2 %%3 %%4 >> %target_dist%\ov-skeleton-generator.cmd
+echo pause                                                   >> %target_dist%\ov-skeleton-generator.cmd
 
-echo @echo off                                               >  %target_dist%\test-ssvep-demo-training.cmd
-echo pushd ..\scripts                                        >> %target_dist%\test-ssvep-demo-training.cmd
-echo call win32-init_env_command.cmd                         >> %target_dist%\test-ssvep-demo-training.cmd
-echo popd                                                    >> %target_dist%\test-ssvep-demo-training.cmd
-echo cd bin                                                  >> %target_dist%\test-ssvep-demo-training.cmd
-echo start OpenViBE-ssvep-demo-dynamic.exe training	         >> %target_dist%\test-ssvep-demo-training.cmd
+echo @echo off                                               >  %target_dist%\ov-ssvep-demo-training.cmd
+echo pushd ..\scripts                                        >> %target_dist%\ov-ssvep-demo-training.cmd
+echo call win32-init_env_command.cmd                         >> %target_dist%\ov-ssvep-demo-training.cmd
+echo popd                                                    >> %target_dist%\ov-ssvep-demo-training.cmd
+echo cd bin                                                  >> %target_dist%\ov-ssvep-demo-training.cmd
+echo start OpenViBE-ssvep-demo-dynamic.exe training          >> %target_dist%\ov-ssvep-demo-training.cmd
 
-echo @echo off                                               >  %target_dist%\test-ssvep-demo-shooter.cmd
-echo pushd ..\scripts                                        >> %target_dist%\test-ssvep-demo-shooter.cmd
-echo call win32-init_env_command.cmd                         >> %target_dist%\test-ssvep-demo-shooter.cmd
-echo popd                                                    >> %target_dist%\test-ssvep-demo-shooter.cmd
-echo cd bin                                                  >> %target_dist%\test-ssvep-demo-shooter.cmd
-echo start OpenViBE-ssvep-demo-dynamic.exe shooter			 >> %target_dist%\test-ssvep-demo-shooter.cmd
+echo @echo off                                               >  %target_dist%\ov-ssvep-demo-shooter.cmd
+echo pushd ..\scripts                                        >> %target_dist%\ov-ssvep-demo-shooter.cmd
+echo call win32-init_env_command.cmd                         >> %target_dist%\ov-ssvep-demo-shooter.cmd
+echo popd                                                    >> %target_dist%\ov-ssvep-demo-shooter.cmd
+echo cd bin                                                  >> %target_dist%\ov-ssvep-demo-shooter.cmd
+echo start OpenViBE-ssvep-demo-dynamic.exe shooter           >> %target_dist%\ov-ssvep-demo-shooter.cmd
 
 echo @echo off                                               >  %target_dist%\bin\OpenViBE-external-application-launcher.cmd
-echo pushd ..			                                     >> %target_dist%\bin\OpenViBE-external-application-launcher.cmd
-echo test-%%1.cmd %%2 %%3 %%4 %%5			                 >> %target_dist%\bin\OpenViBE-external-application-launcher.cmd
-
+echo pushd ..                                                >> %target_dist%\bin\OpenViBE-external-application-launcher.cmd
+echo ov-%%1.cmd %%2 %%3 %%4 %%5                              >> %target_dist%\bin\OpenViBE-external-application-launcher.cmd
 
 echo.
 for /F %%s in (%OpenViBE_build_order%) do (
