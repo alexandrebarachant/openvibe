fcdc7a0cbfa4f1842bf1a5cda51db8c528f9f84e
Laurent Bonnet <dev@openvibe.inria.fr>
2011-09-30 14:56:17 +0000

    cmake-modules:  * FindThirdPartyEmotivAPI.cmake : added a compilation option to delay the load of Edk.dll. removed the dll copy at post-build.
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@3083 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/cmake-modules/FindThirdPartyEmotivAPI.cmake
+++ trunk/cmake-modules/FindThirdPartyEmotivAPI.cmake
@@ -5,7 +5,8 @@
 # ---------------------------------
 IF(WIN32)
 # edkErrorCode.h EmoStateDLL.h
-	FIND_PATH(PATH_EmotivAPI edk.h  PATHS "C:/Program Files/Emotiv/Emotiv Development Kit_v1.0.0.3-PREMIUM" "C:/Program Files/Emotiv Development Kit_v1.0.0.3-PREMIUM" "C:/Program Files/Emotiv/Emotiv Development Kit_v1.0.0.3-PREMIUM/doc/examples/include" "C:/Program Files/Emotiv Development Kit_v1.0.0.3-PREMIUM/doc/examples/include" "C:/Program Files (x86)/Emotiv/Emotiv Development Kit_v1.0.0.3-PREMIUM" "C:/Program Files (x86)/Emotiv Development Kit_v1.0.0.3-PREMIUM" "C:/Program Files (x86)/Emotiv/Emotiv Development Kit_v1.0.0.3-PREMIUM/doc/examples/include" "C:/Program Files (x86)/Emotiv Development Kit_v1.0.0.3-PREMIUM/doc/examples/include" $ENV{OpenViBE_dependencies})
+	#FIND_PATH(PATH_EmotivAPI edk.h  PATHS "C:/Program Files/Emotiv Development Kit_v1.0.0.3-PREMIUM"  "C:/Program Files/Emotiv Development Kit_v1.0.0.3-PREMIUM/doc/examples/include" "C:/Program Files (x86)/Emotiv Development Kit_v1.0.0.3-PREMIUM" "C:/Program Files (x86)/Emotiv Development Kit_v1.0.0.3-PREMIUM/doc/examples/include" $ENV{OpenViBE_dependencies})
+	FIND_PATH(PATH_EmotivAPI edk.h  PATHS "C:/Program Files/Emotiv Development Kit_v1.0.0.3-PREMIUM"  "C:/Program Files/Emotiv Development Kit_v1.0.0.3-PREMIUM/doc/examples/include" "C:\Program Files (x86)\Emotiv Research Edition SDK_v1.0.0.4-PREMIUM" "C:\Program Files (x86)\Emotiv Research Edition SDK_v1.0.0.4-PREMIUM/doc/examples/include" $ENV{OpenViBE_dependencies})
 	IF(PATH_EmotivAPI)
 		MESSAGE(STATUS "  Found Emotiv API...")
 		INCLUDE_DIRECTORIES(${PATH_EmotivAPI})
@@ -13,28 +14,31 @@ IF(WIN32)
 		IF(LIB_EmotivAPI)
 			MESSAGE(STATUS "    [  OK  ] lib ${LIB_EmotivAPI}")
 			TARGET_LINK_LIBRARIES(${PROJECT_NAME}-dynamic ${LIB_EmotivAPI} )
+			# To delay load EDK.dll and redistribute binary
+			TARGET_LINK_LIBRARIES(${PROJECT_NAME}-dynamic Delayimp.lib )
+			
 		ELSE(LIB_EmotivAPI)
 			MESSAGE(STATUS "    [FAILED] lib Emotiv edk.lib")
 		ENDIF(LIB_EmotivAPI)
 
-		# Copying the DLL files at postbuild
-		ADD_CUSTOM_COMMAND(
-				TARGET ${PROJECT_NAME}-dynamic
-				POST_BUILD
-				COMMAND ${CMAKE_COMMAND}
-				ARGS -E copy "${PATH_EmotivAPI}/../bin/edk.dll" "${PROJECT_SOURCE_DIR}/bin"
-				COMMENT "      --->   Copying dll file ${PATH_EmotivAPI}/../bin/edk.dll for the Emotiv EPOC driver."
-			VERBATIM)
-		ADD_CUSTOM_COMMAND(
-				TARGET ${PROJECT_NAME}-dynamic
-				POST_BUILD
-				COMMAND ${CMAKE_COMMAND}
-				ARGS -E copy "${PATH_EmotivAPI}/../bin/edk_utils.dll" "${PROJECT_SOURCE_DIR}/bin"
-				COMMENT "      --->   Copying dll file ${PATH_EmotivAPI}/../bin/edk_utils.dll for the Emotiv EPOC driver."
-			VERBATIM)
+		# Copying the DLL files at postbuild : not needed anymore cf delay load library
+		# ADD_CUSTOM_COMMAND(
+				# TARGET ${PROJECT_NAME}-dynamic
+				# POST_BUILD
+				# COMMAND ${CMAKE_COMMAND}
+				# ARGS -E copy "${PATH_EmotivAPI}/../bin/edk.dll" "${PROJECT_SOURCE_DIR}/bin"
+				# COMMENT "      --->   Copying dll file ${PATH_EmotivAPI}/../bin/edk.dll for the Emotiv EPOC driver."
+			# VERBATIM)
+		# ADD_CUSTOM_COMMAND(
+				# TARGET ${PROJECT_NAME}-dynamic
+				# POST_BUILD
+				# COMMAND ${CMAKE_COMMAND}
+				# ARGS -E copy "${PATH_EmotivAPI}/../bin/edk_utils.dll" "${PROJECT_SOURCE_DIR}/bin"
+				# COMMENT "      --->   Copying dll file ${PATH_EmotivAPI}/../bin/edk_utils.dll for the Emotiv EPOC driver."
+			# VERBATIM)
 		
 		ADD_DEFINITIONS(-DTARGET_HAS_ThirdPartyEmotivAPI)
 	ELSE(PATH_EmotivAPI)
-		MESSAGE(STATUS "  FAILED to find Emotiv API - cmake looked in 'C:/Program Files/Emotiv/Emotiv Development Kit_v1.0.0.3-PREMIUM' 'C:/Program Files/Emotiv Development Kit_v1.0.0.3-PREMIUM' 'C:/Program Files/Emotiv/Emotiv Development Kit_v1.0.0.3-PREMIUM/doc/examples/include' 'C:/Program Files/Emotiv Development Kit_v1.0.0.3-PREMIUM/doc/examples/include' and '$ENV{OpenViBE_dependencies}'")
+		MESSAGE(STATUS "  FAILED to find Emotiv API")
 	ENDIF(PATH_EmotivAPI)
 ENDIF(WIN32)
