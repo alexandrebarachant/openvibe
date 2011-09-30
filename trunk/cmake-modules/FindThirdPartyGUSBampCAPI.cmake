4faaf493f40627b46ae392fc7e35357c376fba89
Laurent Bonnet <dev@openvibe.inria.fr>
2011-09-30 12:57:33 +0000

    cmake-modules:  * FindThirdPartyGUSBampCAPI.cmake : now search into the right path, for compatibility with the latest gtec API version (3.11). Added path for windows 7 64b edition (suffix "(x86)" in path).
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@3079 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/cmake-modules/FindThirdPartyGUSBampCAPI.cmake
+++ trunk/cmake-modules/FindThirdPartyGUSBampCAPI.cmake
@@ -4,11 +4,16 @@
 # Adds include path
 # ---------------------------------
 IF(WIN32)
-	FIND_PATH(PATH_GUSBampCAPI API/gUSBamp.h PATHS "C:/Program Files/gtec/gUSBampCAPI" "C:/Program Files (x86)/gtec/gUSBampCAPI" $ENV{OpenViBE_dependencies})
+	FIND_PATH(PATH_GUSBampCAPI gUSBamp.h PATHS 
+		"C:/Program Files/gtec/gUSBampCAPI/API" 
+		"C:/Program Files (x86)/gtec/gUSBampCAPI/API" 
+		"C:/Program Files/gtec/gUSBampCAPI/API/Win32" 
+		"C:/Program Files (x86)/gtec/gUSBampCAPI/API/Win32"
+		$ENV{OpenViBE_dependencies})
 	IF(PATH_GUSBampCAPI)
 		MESSAGE(STATUS "  Found gUSBampCAPI...")
-		INCLUDE_DIRECTORIES(${PATH_GUSBampCAPI}/API)
-		FIND_LIBRARY(LIB_GUSBampCAPI gUSBamp PATHS ${PATH_GUSBampCAPI}/API )
+		INCLUDE_DIRECTORIES(${PATH_GUSBampCAPI})
+		FIND_LIBRARY(LIB_GUSBampCAPI gUSBamp PATHS ${PATH_GUSBampCAPI})
 		IF(LIB_GUSBampCAPI)
 			MESSAGE(STATUS "    [  OK  ] lib ${LIB_GUSBampCAPI}")
 			TARGET_LINK_LIBRARIES(${PROJECT_NAME}-dynamic ${LIB_GUSBampCAPI} )
@@ -21,8 +26,8 @@ IF(WIN32)
 				TARGET ${PROJECT_NAME}-dynamic
 				POST_BUILD
 				COMMAND ${CMAKE_COMMAND}
-				ARGS -E copy "${PATH_GUSBampCAPI}/API/gUSBamp.dll" "${PROJECT_SOURCE_DIR}/bin"
-				COMMENT "      --->   Copying dll file ${PATH_GUSBampCAPI}/API/gUSBamp.dll for the g.Tec g.USBamp driver."
+				ARGS -E copy "${PATH_GUSBampCAPI}/gUSBamp.dll" "${PROJECT_SOURCE_DIR}/bin"
+				COMMENT "      --->   Copying dll file ${PATH_GUSBampCAPI}/gUSBamp.dll for the g.Tec g.USBamp driver."
 			VERBATIM)
 
 		ADD_DEFINITIONS(-DTARGET_HAS_ThirdPartyGUSBampCAPI)
