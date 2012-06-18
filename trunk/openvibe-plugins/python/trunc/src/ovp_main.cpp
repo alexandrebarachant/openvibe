61921d2ff6fcf56e13bf6dc0926fbfce8caa5056
Jozef Legeny <dev@openvibe.inria.fr>
2012-06-18 15:39:58 +0000

    openvibe-plugins/python: * fixed crash when python directory did not exist on windows
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@3311 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/openvibe-plugins/python/trunc/src/ovp_main.cpp
+++ trunk/openvibe-plugins/python/trunc/src/ovp_main.cpp
@@ -5,6 +5,9 @@
 #include "windows.h"
 #endif
 
+#include <string>
+#include <iostream>
+
 class CPythonInitializer
 {
 public:
@@ -12,9 +15,40 @@ public:
 	~CPythonInitializer(void);
 	OpenViBE::boolean IsPythonAvailable(void);
 private:
+	bool checkPythonPath();
 	//		PyThreadState *m_pMainPyThreadState;
 	OpenViBE::boolean m_bPythonAvailable;
 };
+
+bool CPythonInitializer::checkPythonPath()
+{
+	std::string l_sPath = Py_GetPath();
+
+	int found;
+	found = l_sPath.find_first_of(";");
+	while(found != std::string::npos)
+	{
+		if(found > 0)
+		{
+			std::string l_sFilename = l_sPath.substr(0,found);
+			bool l_bExists = (_access(l_sFilename.c_str(), 0) == 0);
+			if (l_bExists)
+			{
+				//std::cout << "found : " << l_sPath.substr(0,found) << std::endl;
+				return true;
+			}
+			else
+			{
+				//std::cout << "NOT found : " << l_sPath.substr(0,found) << std::endl;
+			}
+		}
+		l_sPath = l_sPath.substr(found+1);
+		found = l_sPath.find_first_of(";");
+	}
+	
+	std::cout << "Python directory not found. You probably have a corrupted python installation!" << std::endl;
+	return false;
+}
 
 CPythonInitializer::CPythonInitializer(void)
 {
@@ -24,11 +58,16 @@ CPythonInitializer::CPythonInitializer(void)
 #ifdef OVP_OS_Windows
 		__try
 		{
+			m_bPythonAvailable = false;
 			if (!Py_IsInitialized())
 			{
-				Py_Initialize();
+				// We do not care about the last file, since it is the OpenViBE runtime path
+				if (checkPythonPath())
+				{
+					Py_Initialize();
+					m_bPythonAvailable = true;
+				}
 			}
-			m_bPythonAvailable = true;
 		}
 		__except (EXCEPTION_EXECUTE_HANDLER)
 		{
