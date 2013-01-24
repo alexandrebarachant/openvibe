b5cc7e8e24cbf80cf3e38f7e98ac0f2a0f2ad259
Guillermo Andrade-Barroso <dev@openvibe.inria.fr>
2013-01-24 10:00:07 +0000

    add config file for test
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@3382 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

---
+++ trunk/test/CTestConfig.cmake
@@ -0,0 +1,10 @@
+#ctest_submit(                                             RETURN_VALUE res)
+
+SET(CTEST_PROJECT_NAME "OpenViBe")
+SET(CTEST_NIGHTLY_START_TIME "20:00:00 EST")
+SET(CTEST_DROP_METHOD "http")
+SET(CTEST_DROP_SITE "cdash.inria.fr")
+SET(CTEST_DROP_LOCATION "/CDash/submit.php?project=OpenViBe")
+SET(CTEST_DROP_SITE_CDASH TRUE)
+
+
