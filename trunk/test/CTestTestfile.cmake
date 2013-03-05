f596f92a4dc9c9dba445cf125d74535ca0548911
Guillermo Andrade-Barroso <dev@openvibe.inria.fr>
2013-03-05 18:00:41 +0000

    Add first real test of openvibe-designer run single scenario with automatic stop adn end at 10 sec. generate a ouput file of sinus signal generator and compare with reference output (generated with revision 3484 on Ubuntu 12.10 x86_64)
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@3489 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/test/CTestTestfile.cmake
+++ trunk/test/CTestTestfile.cmake
@@ -14,16 +14,20 @@
 
 # basic test (just for sample) check that binary directory is readable 
 ADD_TEST(LS_BINARY_PATH "ls" "-all")
+ADD_TEST(PWD_BINARY_PATH "pwd")
+SET( $ENV{PATH}    "${CTEST_BINARY_DIRECTORY}" )
+#SET (CTEST_ENVIRONMENT "PATH=${CTEST_BINARY_DIRECTORY}")
 
 ## -- Other Tests : Place a file named DartTestfile.txt in this path with tests.
+
+SUBDIRS("${CTEST_SOURCE_DIRECTORY}/openvibe-applications/designer/trunc/test")
 SUBDIRS("${CTEST_SOURCE_DIRECTORY}/openvibe/trunc/test")
-SUBDIRS("${CTEST_SOURCE_DIRECTORY}openvibe-kernel-omk/trunc/test")
-SUBDIRS("${CTEST_SOURCE_DIRECTORY}openvibe-modules/trunc/test")
-SUBDIRS("${CTEST_SOURCE_DIRECTORY}openvibe-toolkit/trunc/test")
-SUBDIRS("${CTEST_SOURCE_DIRECTORY}openvibe-plugins/trunc/test")
-SUBDIRS("${CTEST_SOURCE_DIRECTORY}openvibe-scenarios/trunc/test")
-SUBDIRS("${CTEST_SOURCE_DIRECTORY}openvibe-applications/designer/trunc/test")
-SUBDIRS("${CTEST_SOURCE_DIRECTORY}openvibe-externals/trunc/test")
+SUBDIRS("${CTEST_SOURCE_DIRECTORY}/openvibe-kernel-omk/trunc/test")
+SUBDIRS("${CTEST_SOURCE_DIRECTORY}/openvibe-modules/trunc/test")
+SUBDIRS("${CTEST_SOURCE_DIRECTORY}/openvibe-toolkit/trunc/test")
+SUBDIRS("${CTEST_SOURCE_DIRECTORY}/openvibe-plugins/trunc/test")
+SUBDIRS("${CTEST_SOURCE_DIRECTORY}/openvibe-scenarios/trunc/test")
+SUBDIRS("${CTEST_SOURCE_DIRECTORY}/openvibe-externals/trunc/test")
 
 
 
