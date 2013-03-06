64eb9915147c9b3d825add00110d0bc6946f185c
Guillermo Andrade-Barroso <dev@openvibe.inria.fr>
2013-03-06 13:47:17 +0000

    set dir for continuous build
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@3495 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/test/openVibeTests.cmake
+++ trunk/test/openVibeTests.cmake
@@ -56,9 +56,7 @@ IF(${CTEST_SCRIPT_ARG} MATCHES Continuous)
 ENDIF(${CTEST_SCRIPT_ARG} MATCHES Continuous)
 
 IF(${MODEL} MATCHES Continuous)
-#	set(OV-ROOT-DIR              "${CMAKE_CURRENT_SOURCE_DIR}/con.ov")
-	set(OV-ROOT-DIR              "/tmp/con.ov")
-#	file(MAKE_DIRECTORY ${OV-ROOT-DIR})
+	set(OV-ROOT-DIR              "${CMAKE_CURRENT_SOURCE_DIR}")
 ELSE(${MODEL} MATCHES Continuous)
 	# create a temporary directory with very short name to host build
 	exec_program("mktemp" ARGS "--tmpdir -d ov.XXX" OUTPUT_VARIABLE OV-ROOT-DIR)
