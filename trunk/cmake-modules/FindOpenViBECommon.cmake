b411c9c0fa51d3048be3d108236a812deb015672
Jussi Lindgren <dev@openvibe.inria.fr>
2013-04-16 08:50:17 +0000

    build:  * Moved "ovp_global_defines.h" to a more natural location in "openvibe-common/src/". This has the benefit that the file comes to the scope of the build system and becomes visible in IDE source/header lists.  * Commented FindOpenViBEPluginsGlobalDefines.cmake as deprecated. Removed references to it from trunk. Calling FindOpenViBECommon.cmake now suffices. Note that at the time of writing, 15 projects fail to compile if the global header is not available, and they do not do this gracefully. So, its uncertain if the TARGET_HAS_ThirdPartyOpenViBEPluginsGlobalDefines define has much utility: only openvibe toolkit seems to guard with it appropriately (though the compiled toolkit appears useless without the defines).
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@3582 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- trunk/cmake-modules/FindOpenViBECommon.cmake
+++ trunk/cmake-modules/FindOpenViBECommon.cmake
@@ -6,12 +6,13 @@
 
 FIND_PATH(PATH_OPENVIBE_COMMON ov_common_defines.h PATHS ${OV_BASE_DIR}/openvibe-common/src)
 IF(PATH_OPENVIBE_COMMON)
-	MESSAGE(STATUS "  Found openvibe-common ...")
+	MESSAGE(STATUS "  Found openvibe-common...")
 	INCLUDE_DIRECTORIES(${PATH_OPENVIBE_COMMON})
 	
 	ADD_DEPENDENCIES(${PROJECT_NAME} openvibe-common)
 	
 	ADD_DEFINITIONS(-DTARGET_HAS_OpenViBE_Common)
+	ADD_DEFINITIONS(-DTARGET_HAS_ThirdPartyOpenViBEPluginsGlobalDefines)	
 ELSE(PATH_OPENVIBE_COMMON)
 	MESSAGE(STATUS "  FAILED to find openvibe-common...")
 ENDIF(PATH_OPENVIBE_COMMON)
--- trunk/cmake-modules/FindOpenViBEPluginsGlobalDefines.cmake
+++ trunk/cmake-modules/FindOpenViBEPluginsGlobalDefines.cmake
@@ -2,8 +2,11 @@
 # Finds OpenViBEPluginsGlobalDefines
 # Adds library to target
 # Adds include path
+#
+# @deprecated Running FindOpenVibeCommon.cmake is sufficient
+#
 # ---------------------------------
-FIND_PATH(PATH_OpenViBEPluginsGlobalDefines ovp_global_defines.h PATHS ${OV_BASE_DIR}/cmake-modules)
+FIND_PATH(PATH_OpenViBEPluginsGlobalDefines ovp_global_defines.h PATHS ${OV_BASE_DIR}/openvibe-common/src)
 IF(PATH_OpenViBEPluginsGlobalDefines)
 	MESSAGE(STATUS "  Found OpenViBE plugins global defines...")
 	INCLUDE_DIRECTORIES(${PATH_OpenViBEPluginsGlobalDefines})
