8458efb37fc5258ba3c83b4417e3406856276eb4
Jozef Legeny <dev@openvibe.inria.fr>
2013-05-06 14:40:17 +0000

    core/cmake-modules: + added necessary find scripts for the kernel
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@3606 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

---
+++ trunk2/core/cmake-modules/FindThirdPartyBoost_Thread.cmake
@@ -0,0 +1,30 @@
+# ---------------------------------
+# Finds third party boost
+# Adds library to target
+# Adds include path
+# ---------------------------------
+
+IF(UNIX)
+	FIND_LIBRARY(LIB_Boost_Thread NAMES "boost_thread-mt" PATHS ${OV_CUSTOM_DEPENDENCIES_PATH}/lib NO_DEFAULT_PATH)
+	FIND_LIBRARY(LIB_Boost_Thread NAMES "boost_thread-mt" PATHS ${OV_CUSTOM_DEPENDENCIES_PATH}/lib)
+	IF(LIB_Boost_Thread)
+		MESSAGE(STATUS "    [  OK  ] lib ${LIB_Boost_Thread}")
+		TARGET_LINK_LIBRARIES(${PROJECT_NAME} ${LIB_Boost_Thread} )
+	ELSE(LIB_Boost_Thread)
+		MESSAGE(STATUS "    [FAILED] lib boost_thread-mt")
+	ENDIF(LIB_Boost_Thread)
+
+	# For Fedora
+	FIND_LIBRARY(LIB_STANDARD_MODULE_PTHREAD pthread)
+	IF(LIB_STANDARD_MODULE_PTHREAD)
+		MESSAGE(STATUS "  Found pthread...")
+		TARGET_LINK_LIBRARIES(${PROJECT_NAME} ${LIB_STANDARD_MODULE_PTHREAD})
+	ELSE(LIB_STANDARD_MODULE_PTHREAD)
+		MESSAGE(STATUS "  FAILED to find pthread...")
+	ENDIF(LIB_STANDARD_MODULE_PTHREAD)
+ENDIF(UNIX)
+
+IF(WIN32)
+	OV_LINK_BOOST_LIB("thread" ${OV_WIN32_BOOST_VERSION})
+ENDIF(WIN32)
+	
