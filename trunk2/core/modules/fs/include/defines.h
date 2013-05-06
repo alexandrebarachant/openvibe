0047a5d67f5d4c5c4b7abd772fd67613df5b487f
Jozef Legeny <dev@openvibe.inria.fr>
2013-05-06 16:24:29 +0000

    core/modules/fs: + added initial files for the FS module
    core/modules/xml:
    + added initial files for the XML module
    
    
    
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@3615 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

---
+++ trunk2/core/modules/fs/include/defines.h
@@ -0,0 +1,10 @@
+#ifndef __FS_Defines_H__
+#define __FS_Defines_H__
+
+#include <ov_common_defines.h>
+
+#define OV_APPEND_TO_NAMESPACE FS
+#include <ov_common_types.h>
+#undef OV_APPEND_TO_NAMESPACE
+
+#endif // __FS_Defines_H__
---
+++ trunk2/core/modules/xml/include/defines.h
@@ -0,0 +1,12 @@
+#ifndef __XML_Defines_H__
+#define __XML_Defines_H__
+
+#include <ov_common_defines.h>
+
+#define OV_APPEND_TO_NAMESPACE XML
+#include <ov_common_types.h>
+#undef OV_APPEND_TO_NAMESPACE
+
+
+
+#endif // __XML_Defines_H__
