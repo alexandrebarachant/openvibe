1655c273f574636e72cbe63b9e60cf6b05c230ed
Jussi Lindgren <dev@openvibe.inria.fr>
2013-05-13 09:57:41 +0000

    applications/demos:  + Added 'vr-demo'  + Added 'ssvep-demo'
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe@3649 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

---
+++ trunk2/core/applications/demos/ssvep-demo/src/ovassvepCSSVEPFlickeringObject.h
@@ -0,0 +1,32 @@
+#ifndef __OpenViBEApplication_CSSVEPFlickeringObject_H__
+#define __OpenViBEApplication_CSSVEPFlickeringObject_H__
+
+#include <ov_all.h>
+#include <ovtk_all.h>
+#include <Ogre.h>
+
+namespace OpenViBESSVEP
+{
+	class CSSVEPFlickeringObject
+	{
+		public:
+			CSSVEPFlickeringObject( Ogre::SceneNode* poObjectNode, OpenViBE::uint32 ui32LitFrames, OpenViBE::uint32 ui32DarkFrames );
+			~CSSVEPFlickeringObject() {};
+
+			virtual void setVisible( OpenViBE::boolean bVisibility );
+			virtual void processFrame(  );
+
+		protected:
+			Ogre::SceneNode* m_poObjectNode;
+			OpenViBE::uint32 m_ui32CurrentFrame;
+			OpenViBE::uint32 m_ui32LitFrames;
+			OpenViBE::uint32 m_ui32DarkFrames;
+
+		private:
+
+			OpenViBE::boolean m_bVisible;
+	};
+}
+
+
+#endif // __OpenViBEApplication_CSSVEPFlickeringObject_H__
---
+++ trunk2/core/applications/demos/ssvep-demo/src/ovassvep_defines.h
@@ -0,0 +1,5 @@
+#ifndef __OpenViBEApplication_Defines_H__
+#define __OpenViBEApplication_Defines_H__
+
+
+#endif // __OpenViBEApplication_Defines_H__
